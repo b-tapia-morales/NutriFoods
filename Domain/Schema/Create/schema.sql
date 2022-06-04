9c4e33dd6438833a7c327c862ba25ba19375476b
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-06-04 14:43:30 -0400

    Added initial schema script

---
+++ Domain/Schema/Create/schema.sql
@@ -0,0 +1,335 @@
+DROP TABLE IF EXISTS nutrifoods.menu_recipe;
+DROP TABLE IF EXISTS nutrifoods.meal_menu;
+DROP TABLE IF EXISTS nutrifoods.meal_plan;
+DROP TABLE IF EXISTS nutrifoods.user_allergy;
+DROP TABLE IF EXISTS nutrifoods.user_metric;
+DROP TABLE IF EXISTS nutrifoods.user_profile;
+DROP TABLE IF EXISTS nutrifoods.ingredient_quantity;
+DROP TABLE IF EXISTS nutrifoods.ingredient_measure;
+DROP TABLE IF EXISTS nutrifoods.recipe_nutrient;
+DROP TABLE IF EXISTS nutrifoods.recipe_steps;
+DROP TABLE IF EXISTS nutrifoods.recipe_diet;
+DROP TABLE IF EXISTS nutrifoods.recipe_meal_type;
+DROP TABLE IF EXISTS nutrifoods.recipe_dish_type;
+DROP TABLE IF EXISTS nutrifoods.recipe_section;
+DROP TABLE IF EXISTS nutrifoods.recipe;
+DROP TABLE IF EXISTS nutrifoods.measure;
+DROP TABLE IF EXISTS nutrifoods.ingredient_nutrient;
+DROP TABLE IF EXISTS nutrifoods.ingredient;
+DROP TABLE IF EXISTS nutrifoods.nutrient;
+DROP TABLE IF EXISTS nutrifoods.nutrient_subtype;
+DROP TABLE IF EXISTS nutrifoods.nutrient_type;
+DROP TABLE IF EXISTS nutrifoods.diet;
+DROP TABLE IF EXISTS nutrifoods.dish_type;
+DROP TABLE IF EXISTS nutrifoods.meal_type;
+DROP TABLE IF EXISTS nutrifoods.tertiary_group;
+DROP TABLE IF EXISTS nutrifoods.secondary_group;
+DROP TABLE IF EXISTS nutrifoods.primary_group;
+
+DROP SCHEMA IF EXISTS nutrifoods;
+
+CREATE SCHEMA IF NOT EXISTS nutrifoods;
+
+SET search_path = "nutrifoods";
+
+CREATE TABLE IF NOT EXISTS primary_group
+(
+    id   SERIAL,
+    name VARCHAR(64) NOT NULL,
+    UNIQUE (name),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS secondary_group
+(
+    id               SERIAL,
+    name             VARCHAR(64) NOT NULL,
+    primary_group_id INTEGER     NOT NULL,
+    FOREIGN KEY (primary_group_id) REFERENCES primary_group (id),
+    UNIQUE (name),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS tertiary_group
+(
+    id                 SERIAL      NOT NULL,
+    name               VARCHAR(64) NOT NULL,
+    secondary_group_id INTEGER     NOT NULL,
+    FOREIGN KEY (secondary_group_id) REFERENCES secondary_group (id),
+    UNIQUE (name),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS dish_type
+(
+    id   SERIAL,
+    name VARCHAR(64) NOT NULL,
+    UNIQUE (name),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS meal_type
+(
+    id   SERIAL,
+    name VARCHAR(64) NOT NULL,
+    UNIQUE (name),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS diet
+(
+    id   SERIAL,
+    name VARCHAR(64) NOT NULL,
+    UNIQUE (name),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS nutrient_type
+(
+    id   SERIAL,
+    name VARCHAR(64) NOT NULL,
+    UNIQUE (name),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS nutrient_subtype
+(
+    id              SERIAL,
+    name            VARCHAR(64) NOT NULL,
+    provides_energy BOOLEAN     NOT NULL,
+    type_id         INTEGER     NOT NULL,
+    FOREIGN KEY (type_id) REFERENCES nutrient_type (id),
+    UNIQUE (name),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS nutrient
+(
+    id            SERIAL,
+    name          VARCHAR(64) NOT NULL,
+    also_called   VARCHAR(64),
+    is_calculated BOOLEAN     NOT NULL,
+    essentiality  INTEGER     NOT NULL,
+    subtype_id    INTEGER     NOT NULL,
+    FOREIGN KEY (subtype_id) REFERENCES nutrient_subtype (id),
+    UNIQUE (name),
+    UNIQUE (also_called),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS ingredient
+(
+    id                SERIAL,
+    name              VARCHAR(64) NOT NULL,
+    processing        INTEGER     NOT NULL,
+    is_animal         BOOLEAN     NOT NULL,
+    contains_gluten   BOOLEAN     NOT NULL,
+    tertiary_group_id INTEGER     NOT NULL,
+    UNIQUE (name),
+    FOREIGN KEY (tertiary_group_id) REFERENCES tertiary_group (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS ingredient_nutrient
+(
+    id            SERIAL,
+    ingredient_id INTEGER          NOT NULL,
+    nutrient_id   INTEGER          NOT NULL,
+    quantity      DOUBLE PRECISION NOT NULL,
+    unit          INTEGER,
+    FOREIGN KEY (ingredient_id) REFERENCES ingredient (id),
+    FOREIGN KEY (nutrient_id) REFERENCES nutrient (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS measure
+(
+    id            SERIAL,
+    ingredient_id INTEGER          NOT NULL,
+    name          VARCHAR(64)      NOT NULL,
+    grams         DOUBLE PRECISION NOT NULL,
+    UNIQUE (ingredient_id, name),
+    FOREIGN KEY (ingredient_id) REFERENCES ingredient (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS recipe
+(
+    id               SERIAL,
+    name             VARCHAR(64) NOT NULL,
+    author           VARCHAR(64) NOT NULL,
+    url              TEXT,
+    portions         INTEGER,
+    preparation_time INTEGER,
+    UNIQUE (name, author),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS recipe_section
+(
+    id   SERIAL,
+    name VARCHAR(64) NOT NULL,
+    UNIQUE (name),
+    FOREIGN KEY (id) REFERENCES recipe (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS recipe_dish_type
+(
+    id           SERIAL,
+    recipe_id    INTEGER NOT NULL,
+    dish_type_id INTEGER NOT NULL,
+    FOREIGN KEY (recipe_id) REFERENCES recipe (id),
+    FOREIGN KEY (dish_type_id) REFERENCES dish_type (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS recipe_meal_type
+(
+    id           SERIAL,
+    recipe_id    INTEGER NOT NULL,
+    meal_type_id INTEGER NOT NULL,
+    FOREIGN KEY (recipe_id) REFERENCES recipe (id),
+    FOREIGN KEY (meal_type_id) REFERENCES meal_type (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS recipe_diet
+(
+    id        SERIAL,
+    recipe_id INTEGER NOT NULL,
+    diet_id   INTEGER NOT NULL,
+    FOREIGN KEY (recipe_id) REFERENCES recipe (id),
+    FOREIGN KEY (diet_id) REFERENCES diet (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS recipe_steps
+(
+    id                SERIAL,
+    recipe_section_id INTEGER NOT NULL,
+    step              INTEGER NOT NULL,
+    description       TEXT,
+    FOREIGN KEY (recipe_section_id) REFERENCES recipe_section (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS recipe_nutrient
+(
+    id          SERIAL,
+    recipe_id   INTEGER          NOT NULL,
+    nutrient_id INTEGER          NOT NULL,
+    quantity    DOUBLE PRECISION NOT NULL,
+    unit        INTEGER,
+    FOREIGN KEY (recipe_id) REFERENCES recipe (id),
+    FOREIGN KEY (nutrient_id) REFERENCES nutrient (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS ingredient_measure
+(
+    id                SERIAL,
+    recipe_section_id INTEGER NOT NULL,
+    measure_id        INTEGER NOT NULL,
+    integer_part      INTEGER NOT NULL,
+    numerator         INTEGER,
+    denominator       INTEGER,
+    description       TEXT,
+    FOREIGN KEY (recipe_section_id) REFERENCES recipe_section (id),
+    FOREIGN KEY (measure_id) REFERENCES measure (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS ingredient_quantity
+(
+    id                SERIAL,
+    recipe_section_id INTEGER          NOT NULL,
+    ingredient_id     INTEGER          NOT NULL,
+    quantity          DOUBLE PRECISION NOT NULL,
+    description       TEXT,
+    FOREIGN KEY (recipe_section_id) REFERENCES recipe_section (id),
+    FOREIGN KEY (ingredient_id) REFERENCES ingredient (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_profile
+(
+    id        SERIAL,
+    username  VARCHAR(64) NOT NULL,
+    email     TEXT        NOT NULL,
+    password  TEXT        NOT NULL,
+    api_key   TEXT        NOT NULL,
+    name      VARCHAR(64),
+    last_name VARCHAR(64),
+    birthdate DATE        NOT NULL,
+    gender    INTEGER     NOT NULL,
+    joined_on TIMESTAMP   NOT NULL,
+    UNIQUE (username),
+    UNIQUE (email),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_metric
+(
+    id                      SERIAL,
+    user_id                 INTEGER          NOT NULL,
+    height                  DOUBLE PRECISION NOT NULL,
+    weight                  DOUBLE PRECISION NOT NULL,
+    body_mass_index         DOUBLE PRECISION NOT NULL,
+    muscle_mass_percentage  DOUBLE PRECISION,
+    physical_activity_level INTEGER          NOT NULL,
+    diet_id                 INTEGER,
+    FOREIGN KEY (user_id) REFERENCES user_profile (id),
+    FOREIGN KEY (diet_id) REFERENCES diet (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_allergy
+(
+    id            SERIAL,
+    user_id       INTEGER NOT NULL,
+    ingredient_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_profile (id),
+    FOREIGN KEY (ingredient_id) REFERENCES ingredient (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS meal_plan
+(
+    id                   SERIAL,
+    user_id              INTEGER          NOT NULL,
+    meals_daily_amount   INTEGER          NOT NULL,
+    energy_target        DOUBLE PRECISION NOT NULL,
+    carbohydrates_target DOUBLE PRECISION NOT NULL,
+    lipids_target        DOUBLE PRECISION NOT NULL,
+    proteins_target      DOUBLE PRECISION NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_profile (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS meal_menu
+(
+    id                  SERIAL,
+    meal_plan_id        INTEGER          NOT NULL,
+    meal_type_id        INTEGER          NOT NULL,
+    satiety             INTEGER          NOT NULL,
+    energy_total        DOUBLE PRECISION NOT NULL,
+    carbohydrates_total DOUBLE PRECISION NOT NULL,
+    lipids_total        DOUBLE PRECISION NOT NULL,
+    proteins_total      DOUBLE PRECISION NOT NULL,
+    alcohol_total       DOUBLE PRECISION NOT NULL,
+    FOREIGN KEY (meal_plan_id) REFERENCES meal_plan (id),
+    FOREIGN KEY (meal_type_id) REFERENCES meal_type (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS menu_recipe
+(
+    id           SERIAL,
+    meal_menu_id INTEGER NOT NULL,
+    recipe_id    INTEGER NOT NULL,
+    quantity     INTEGER NOT NULL,
+    FOREIGN KEY (meal_menu_id) REFERENCES meal_menu (id),
+    FOREIGN KEY (recipe_id) REFERENCES recipe (id),
+    PRIMARY KEY (meal_menu_id, recipe_id)
+);
