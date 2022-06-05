31867d7668bf2b23f39fafdc7362fa92c29b0e76
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-06-05 16:15:11 -0400

    Added dependency to read csv files

--- Domain/ApiRetrieval/ApiRetrieval.cs
+++ Domain/ApiRetrieval/ApiRetrieval.cs
@@ -1,3 +1,7 @@
+using System.Globalization;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Domain.Models;
 
 namespace Domain.ApiRetrieval;
@@ -15,9 +19,35 @@ public class ApiRetrieval
     {
         var currentDirectory = Directory.GetCurrentDirectory();
         var path = Path.Combine(currentDirectory, "Files", "NutrientIds.csv");
-        using var streamReader = new StreamReader(path);
-        Console.WriteLine(streamReader.ToString());
-        return null;
+        var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Encoding = Encoding.UTF8,
+            Delimiter = ";" ,
+            HasHeaderRecord = true
+        };
+
+        using var fileStream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+        using var textReader = new StreamReader(fileStream, Encoding.UTF8);
+        using var csv = new CsvReader(textReader, configuration);
+
+        var dictionary = new Dictionary<int, int>();
+        var data = csv.GetRecords<CsvRow>();
+
+        foreach (var row in data)
+        {
+            Console.WriteLine(row);
+            dictionary.Add(row.FoodDataCentralId, row.NutriFoodsId);
+            Console.WriteLine($"{row.FoodDataCentralId}, {row.NutriFoodsId}");
+        }
+
+        return dictionary;
     }
 
+    private class CsvRow
+    {
+        public string FoodDataCentralName { get; set; }
+        public int FoodDataCentralId { get; set; }
+        public string NutriFoodsName { get; set; }
+        public int NutriFoodsId { get; set; }
+    }
 }
