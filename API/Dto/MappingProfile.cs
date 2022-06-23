f85b278af7a2815358a992f51c4d88f5b9c446ff
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-06-22 20:35:13 -0400

    Added basic mappings for enumeration tables

--- API/Dto/MappingProfile.cs
+++ API/Dto/MappingProfile.cs
@@ -1,6 +1,22 @@
+using AutoMapper;
+using Domain.Models;
+
 namespace API.Dto;
 
-public class MappingProfile
+public class MappingProfile : Profile
 {
-    
+    public MappingProfile()
+    {
+        CreateMap<Diet, DietDto>();
+        CreateMap<DishType, DishTypeDto>();
+        CreateMap<MealType, MealTypeDto>();
+        CreateMap<NutrientType, NutrientTypeDto>();
+        CreateMap<NutrientSubtype, NutrientSubtypeDto>();
+        CreateMap<Nutrient, NutrientDto>()
+            .ForMember(e => e.Essentiality, 
+                opt => opt.MapFrom(src => src.Essentiality.NameDisplay));
+        CreateMap<PrimaryGroup, PrimaryGroupDto>();
+        CreateMap<SecondaryGroup, SecondaryGroupDto>();
+        CreateMap<TertiaryGroup, TertiaryGroupDto>();
+    }
 }
