622094785bcde7f1cc3825bf91529a1ab7c01713
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-06-29 20:03:17 -0400

    Moved abridged food classes to the same namespace

--- NutrientRetrieval/Model/Food.cs
+++ NutrientRetrieval/Model/Food.cs
@@ -3,14 +3,35 @@ namespace NutrientRetrieval.Model;
 public class Food
 {
     public int FdcId { get; set; }
-    public string Description { get; set; }
-    public FoodNutrient[] FoodNutrients { get; set; }
+    public string Description { get; set; } = null!;
+    public FoodNutrient[] FoodNutrients { get; set; } = null!;
+    public FoodPortion[] FoodPortions { get; set; } = null!;
+    public int NdbNumber { get; set; }
+}
 
-    public override string ToString()
-    {
-        return $@"
-{FdcId}
-{Description}
-{string.Join(Environment.NewLine, (IEnumerable<FoodNutrient>) FoodNutrients)}";
-    }
+public class FoodNutrient
+{
+    public Nutrient Nutrient { get; set; } = null!;
+    public string Type { get; set; } = null!;
+    public int Id { get; set; }
+    public float Amount { get; set; }
+    public int DataPoints { get; set; }
+}
+
+public class Nutrient
+{
+    public int Id { get; set; }
+    public string Number { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public int Rank { get; set; }
+    public string UnitName { get; set; } = null!;
+}
+
+public class FoodPortion
+{
+    public int Id { get; set; }
+    public float GramWeight { get; set; }
+    public int SequenceNumber { get; set; }
+    public int Amount { get; set; }
+    public string Modifier { get; set; }
 }
