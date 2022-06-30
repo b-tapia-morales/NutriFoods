e4c796a50a168b790966150be01a36b447007f07
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-06-29 22:55:26 -0400

    Excluded unused classes from JSON response

--- NutrientRetrieval/Model/Food.cs
+++ NutrientRetrieval/Model/Food.cs
@@ -1,68 +1,28 @@
-using System.Text.Json.Serialization;
-
 namespace NutrientRetrieval.Model;
 
 public class Food
 {
     public int FdcId { get; set; }
     public string Description { get; set; } = null!;
-    [JsonIgnore] public string PublicationDate { get; set; } = null!;
-    public Foodnutrient[] FoodNutrients { get; set; } = null!;
-    public Foodportion[] FoodPortions { get; set; } = null!;
-    [JsonIgnore]
-    public string DataType { get; set; } = null!;
-    [JsonIgnore]
-    public string FoodClass { get; set; } = null!;
-    [JsonIgnore]
-    public string ScientificName { get; set; } = null!;
-    [JsonIgnore]
-    public object[] FoodComponents { get; set; } = null!;
-    [JsonIgnore]
-    public object[] FoodAttributes { get; set; } = null!;
-    [JsonIgnore]
-    public Nutrientconversionfactor[] NutrientConversionFactors { get; set; } = null!;
-    [JsonIgnore]
-    public object[] InputFoods { get; set; } = null!;
+    public FoodNutrient[] FoodNutrients { get; set; } = null!;
+    public FoodPortion[] FoodPortions { get; set; } = null!;
     public int NdbNumber { get; set; }
-    [JsonIgnore]
-    public bool IsHistoricalReference { get; set; }
-    [JsonIgnore]
-    public Foodcategory FoodCategory { get; set; } = null!;
-    
+
     public override string ToString()
     {
         return $@"
 {FdcId}
 {Description}
 {NdbNumber}
-{string.Join(Environment.NewLine, (IEnumerable<Foodportion>) FoodPortions)}";
+{string.Join(Environment.NewLine, (IEnumerable<FoodPortion>) FoodPortions)}";
     }
 }
 
-public class Foodcategory
-{
-    public int Id { get; set; }
-    public string Code { get; set; } = null!;
-    public string Description { get; set; } = null!;
-}
-
-public class Foodnutrient
+public class FoodNutrient
 {
     public Nutrient Nutrient { get; set; } = null!;
-    [JsonIgnore]
-    public string Type { get; set; } = null!;
-    [JsonIgnore]
-    public Foodnutrientderivation FoodNutrientDerivation { get; set; } = null!;
-    [JsonIgnore]
-    public int Id { get; set; }
-    public float Amount { get; set; }
-    [JsonIgnore]
-    public int DataPoints { get; set; }
-    [JsonIgnore]
-    public float Max { get; set; }
-    [JsonIgnore]
-    public float Min { get; set; }
-    
+    public double Amount { get; set; }
+
     public override string ToString()
     {
         return $@"
@@ -76,7 +36,6 @@ public class Nutrient
     public int Id { get; set; }
     public string Number { get; set; } = null!;
     public string Name { get; set; } = null!;
-    public int Rank { get; set; }
     public string UnitName { get; set; } = null!;
     
     public override string ToString()
@@ -89,30 +48,11 @@ public class Nutrient
     }
 }
 
-public class Foodnutrientderivation
+public class FoodPortion
 {
-    public int Id { get; set; }
-    public string Code { get; set; } = null!;
-    public string Description { get; set; } = null!;
-    public Foodnutrientsource FoodNutrientSource { get; set; } = null!;
-}
-
-public class Foodnutrientsource
-{
-    public int Id { get; set; }
-    public string Code { get; set; } = null!;
-    public string Description { get; set; } = null!;
-}
-
-public class Foodportion
-{
-    public int Id { get; set; }
     public double GramWeight { get; set; }
-    public int SequenceNumber { get; set; }
     public double Amount { get; set; }
     public string Modifier { get; set; } = null!;
-    [JsonIgnore]
-    public Measureunit MeasureUnit { get; set; } = null!;
     
     public override string ToString()
     {
@@ -121,22 +61,4 @@ public class Foodportion
 {Amount}
 {GramWeight}";
     }
-}
-
-public class Measureunit
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string Abbreviation { get; set; } = null!;
-}
-
-public class Nutrientconversionfactor
-{
-    public int Id { get; set; }
-    public float ProteinValue { get; set; }
-    public float FatValue { get; set; }
-    public float CarbohydrateValue { get; set; }
-    public string Type { get; set; }
-    public string Name { get; set; }
-    public float Value { get; set; }
 }
