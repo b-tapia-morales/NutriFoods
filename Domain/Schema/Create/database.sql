97a0bc7ee9d281c01dc47a057f8134d1e45defd4
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-07-05 23:52:00 -0400

    Added "if exists" to drop command

--- Domain/Schema/Create/database.sql
+++ Domain/Schema/Create/database.sql
@@ -1,10 +1,13 @@
 ﻿CREATE DATABASE nutrifoods_db;
+
 CREATE USER nutrifoods_dev WITH ENCRYPTED PASSWORD 'MVmYneLqe91$';
-GRANT ALL PRIVILEGES ON DATABASE nutrifoods_db TO nutrifoods_dev;
 GRANT USAGE ON SCHEMA nutrifoods TO nutrifoods_dev;
-GRANT SELECT ON ALL TABLES IN SCHEMA nutrifoods TO nutrifoods_dev;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA nutrifoods TO nutrifoods_dev;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA nutrifoods TO nutrifoods_dev;
+GRANT ALL PRIVILEGES ON DATABASE nutrifoods_db TO nutrifoods_dev;
+
+DROP EXTENSION IF EXISTS fuzzystrmatch;
 CREATE EXTENSION fuzzystrmatch;
-create extension pg_trgm;
-drop EXTENSION pg_trgm;
+
+DROP EXTENSION IF EXISTS pg_trgm;
+CREATE EXTENSION pg_trgm;
