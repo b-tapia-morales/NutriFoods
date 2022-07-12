af64d5f83963eb67f534aa4404ebac059476c550
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-07-12 15:30:45 -0400

    Separated csv classes into their own namespaces

--- NutrientRetrieval/Dictionaries/IngredientDictionary.cs
+++ NutrientRetrieval/Dictionaries/IngredientDictionary.cs
@@ -24,21 +24,4 @@ public static class IngredientDictionary
         return csv.GetRecords<IngredientRow>()
             .ToDictionary(record => record.NutriFoodsId, record => record.FoodDataCentralId);
     }
-
-    public sealed class IngredientRow
-    {
-        public int NutriFoodsId { get; set; } = 0;
-        public string NutriFoodsName { get; set; } = null!;
-        public int FoodDataCentralId { get; set; } = 0;
-    }
-
-    public sealed class IngredientMapping : ClassMap<IngredientRow>
-    {
-        public IngredientMapping()
-        {
-            Map(p => p.NutriFoodsId).Index(0);
-            Map(p => p.NutriFoodsName).Index(1);
-            Map(p => p.FoodDataCentralId).Index(2).Optional();
-        }
-    }
 }
--- NutrientRetrieval/Dictionaries/NutrientDictionary.cs
+++ NutrientRetrieval/Dictionaries/NutrientDictionary.cs
@@ -24,23 +24,4 @@ public static class NutrientDictionary
         return csv.GetRecords<NutrientRow>()
             .ToDictionary(record => record.FoodDataCentralId, record => record.NutriFoodsId);
     }
-
-    private sealed class NutrientRow
-    {
-        public string FoodDataCentralName { get; set; } = null!;
-        public string FoodDataCentralId { get; set; } = null!;
-        public string NutriFoodsName { get; set; } = null!;
-        public int NutriFoodsId { get; set; } = 0;
-    }
-
-    private sealed class NutrientMapping : ClassMap<NutrientRow>
-    {
-        public NutrientMapping()
-        {
-            Map(p => p.FoodDataCentralName).Index(0);
-            Map(p => p.FoodDataCentralId).Index(1);
-            Map(p => p.NutriFoodsName).Index(2).Optional();
-            Map(p => p.NutriFoodsId).Index(3).Optional();
-        }
-    }
 }
