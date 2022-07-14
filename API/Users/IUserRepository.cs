203031223a9e03f6ac5fa4a32543fcbc88d12438
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-07-13 23:19:12 -0400

    Added basic user creation and validation functions

--- API/Users/IUserRepository.cs
+++ API/Users/IUserRepository.cs
@@ -1,12 +1,17 @@
+using API.Dto;
+using Domain.Enum;
 using Domain.Models;
 
 namespace API.Users;
 
 public interface IUserRepository
 {
-    Task<UserProfile> Find(string apiKey);
+    Task<UserDto?> Find(string apiKey);
 
-    Task<UserProfile> FindByUsername(string username, string password);
+    Task<UserDto?> FindByUsername(string username, string password);
 
-    Task<UserProfile> FindByEmail(string email, string password);
+    Task<UserDto?> FindByEmail(string email, string password);
+
+    public Task<UserDto?> SaveUser(string username, string email, string password, string? name, string? lastName,
+        DateOnly birthDate, Gender gender);
 }
