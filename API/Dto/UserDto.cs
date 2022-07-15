8ebb245ccb5e3a69e7925929ce2b94182039a277
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-07-15 15:55:14 -0400

    Updated classes related to user controller

--- API/Dto/UserDto.cs
+++ API/Dto/UserDto.cs
@@ -2,16 +2,17 @@ namespace API.Dto;
 
 public class UserDto
 {
+    public int Id { get; set; }
     public string Username { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
     public string ApiKey { get; set; } = null!;
     public string? Name { get; set; }
     public string? LastName { get; set; }
-    public string Birthdate { get; set; }
+    public string Birthdate { get; set; } = null!;
     public string Gender { get; set; } = null!;
     public DateTime JoinedOn { get; set; }
     public DietDto? Diet { get; set; }
     public MealPlanDto? MealPlan { get; set; }
-    public IEnumerable<UserBodyMetricDto> BodyMetrics { get; set; } = null!;
+    public ICollection<UserBodyMetricDto> BodyMetrics { get; set; } = null!;
 }
--- API/Users/IUserRepository.cs
+++ API/Users/IUserRepository.cs
@@ -13,4 +13,7 @@ public interface IUserRepository
 
     public Task<UserDto?> SaveUser(string username, string email, string password, string? name, string? lastName,
         DateOnly birthDate, Gender gender);
+
+    public Task<UserDto?> SaveBodyMetrics(string apiKey, int height, double weight, PhysicalActivity level,
+        double? muscleMassPercentage);
 }
