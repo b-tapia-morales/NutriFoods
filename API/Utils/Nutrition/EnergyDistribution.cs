b311a47b97bfc961a3f85e2e7b275b732cb47a16
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-07-17 19:37:45 -0400

    Fixed bug calculating energy distribution: now also calculating with percentages

--- API/Utils/Nutrition/EnergyDistribution.cs
+++ API/Utils/Nutrition/EnergyDistribution.cs
@@ -3,12 +3,18 @@ using static API.Utils.Nutrition.Macronutrient;
 
 namespace API.Utils.Nutrition;
 
-public class EnergyDistribution
+public static class EnergyDistribution
 {
-    public (double Carbohydrates, double Lipids, double Proteins, double Alcohol) Calculate(double energy)
+    private const double CarbohydratesPercentage = 0.6;
+    private const double LipidsPercentage = 0.25;
+    private const double ProteinsPercentage = 0.15;
+
+    public static (double Carbohydrates, double Lipids, double Proteins) Calculate(double energy,
+        double carbohydratesPercentage = CarbohydratesPercentage, double lipidsPercentage = LipidsPercentage,
+        double proteinsPercentage = ProteinsPercentage)
     {
-        return (energy / Carbohydrates.Multiplier, energy / Lipids.Multiplier, energy / Proteins.Multiplier,
-            energy / Alcohol.Multiplier);
+        return ((energy / Carbohydrates.Multiplier) * carbohydratesPercentage,
+            (energy / Lipids.Multiplier) * lipidsPercentage, (energy / Proteins.Multiplier) * proteinsPercentage);
     }
 }
 
@@ -16,6 +22,7 @@ public sealed class Macronutrient : SmartEnum<Macronutrient>
 {
     public static readonly Macronutrient
         Carbohydrates = new(nameof(Carbohydrates), 1, "Carbohidratos", 4);
+
     public static readonly Macronutrient Lipids = new(nameof(Lipids), 2, "Lípidos", 9);
     public static readonly Macronutrient Proteins = new(nameof(Proteins), 1, "Proteínas", 4);
     public static readonly Macronutrient Alcohol = new(nameof(Alcohol), 1, "Alcohol", 7);
