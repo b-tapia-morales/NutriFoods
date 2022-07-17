90d5a70504c96bd858d4f19bbe6a8b50293a0478
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-07-17 19:37:24 -0400

    Added new math utils helper class

---
+++ API/Utils/MathUtils.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Utils;
+
+public static class MathUtils
+{
+    public static int Gcd(IEnumerable<int> numbers)
+    {
+        return numbers.Aggregate(Gcd);
+    }
+    
+    public static int Gcd(int a, int b)
+    {
+        while (true)
+        {
+            if (a == 0 || b == 0) return a | b;
+            a = Math.Min(a, b);
+            b = Math.Max(a, b) % Math.Min(a, b);
+        }
+    }
+}
