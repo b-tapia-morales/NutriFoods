1a7025c2fb14772fea8500dbb0212d05cf107e63
bairon-tapia <btapiam@alumnosuls.cl>
2022-07-27 01:42:41 -0400

    Added utility method to partition an enumerable into equal chunks

---
+++ Utils/Enumerable/EnumerableUtils.cs
@@ -0,0 +1,24 @@
+namespace Utils.Enumerable;
+
+public static class EnumerableUtils
+{
+    public static IEnumerable<IEnumerable<T>> Partition<T>(IEnumerable<T> source, int size)
+    {
+        var partition = new List<T>(size);
+        var counter = 0;
+
+        using var enumerator = source.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            partition.Add(enumerator.Current);
+            counter++;
+            if (counter % size != 0) continue;
+            yield return partition.ToList();
+            partition.Clear();
+            counter = 0;
+        }
+
+        if (counter != 0)
+            yield return partition;
+    }
+}
