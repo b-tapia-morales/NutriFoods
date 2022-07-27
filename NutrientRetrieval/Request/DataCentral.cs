74f68c03848e0a28b793d203910c0b3691cea03d
bairon-tapia <btapiam@alumnosuls.cl>
2022-07-27 02:07:57 -0400

    Removed all nullable references, also renamed methods to describe what they do in a better way

--- NutrientRetrieval/Request/DataCentral.cs
+++ NutrientRetrieval/Request/DataCentral.cs
@@ -12,7 +12,7 @@ public static class DataCentral
 
     private static readonly HttpClient Client = new();
 
-    public static async Task<(int Id, T? Food)> SingleRequest<T>(int nutriFoodsId, int foodDataCentralId, string format)
+    public static async Task<(int Id, T Food)> FetchItem<T>(int nutriFoodsId, int foodDataCentralId, string format)
         where T : IFood
     {
         var path = $"https://api.nal.usda.gov/fdc/v1/food/{foodDataCentralId}?format={format}&api_key={ApiKey}";
@@ -20,12 +20,12 @@ public static class DataCentral
 
         var response = await Client.GetAsync(uri);
         var serialized = await response.Content.ReadAsStringAsync();
-        var food = JsonConvert.DeserializeObject<T>(serialized);
+        var food = JsonConvert.DeserializeObject<T>(serialized) ?? throw new InvalidOperationException();
 
         return (nutriFoodsId, food);
     }
 
-    public static async Task<IEnumerable<(int Id, T Food)>> MultipleRequests<T>(
+    public static async Task<IEnumerable<(int Id, T Food)>> FetchList<T>(
         Dictionary<int, int> dictionary, string format) where T : IFood
     {
         var ids = string.Join('&', dictionary.Select(e => $"fdcIds={e.Key}"));
@@ -39,22 +39,44 @@ public static class DataCentral
         return list.Select(e => (dictionary[e.FdcId()], e));
     }
 
-    public static async Task<Dictionary<int, T?>> SingleRetrieval<T>(string format) where T : IFood
+    public static async Task<Dictionary<int, T>> RetrieveByItem<T>(string format) where T : IFood
     {
         var dictionary = IngredientRetrieval.RetrieveRows()
             .ToDictionary(e => e.NutriFoodsId, e => e.FoodDataCentralId);
-        var tasks = dictionary.Select(e => SingleRequest<T>(e.Key, e.Value, format));
+        var tasks = dictionary.Select(e => FetchItem<T>(e.Key, e.Value, format));
         var tuples = await Task.WhenAll(tasks);
         return tuples.ToDictionary(tuple => tuple.Id, tuple => tuple.Food);
     }
 
-    public static async Task<Dictionary<int, T>> MultipleRetrievals<T>(string format) where T : IFood
+    public static async Task<Dictionary<int, T>> RetrieveByList<T>(string format) where T : IFood
     {
         var dictionary = IngredientRetrieval.RetrieveRows()
+            .DistinctBy(e => e.FoodDataCentralId)
             .ToDictionary(e => e.FoodDataCentralId, e => e.NutriFoodsId);
         var dictionaryList = EnumerableUtils.Partition(dictionary, MaxItemsPerRequest);
-        var tasks = dictionaryList.Select(e => MultipleRequests<T>(new Dictionary<int, int>(e), format));
+        var tasks = dictionaryList.Select(e => FetchList<T>(new Dictionary<int, int>(e), format));
         var tuplesList = await Task.WhenAll(tasks);
         return tuplesList.SelectMany(e => e).ToDictionary(e => e.Id, e => e.Food);
     }
+
+    public static async Task<Dictionary<int, T>> PerformRequest<T>(RequestMethod method, string format) where T : IFood
+    {
+        return method switch
+        {
+            RequestMethod.Single => await RetrieveByItem<T>(format),
+            RequestMethod.Multiple => await RetrieveByList<T>(format),
+            _ => throw new ArgumentOutOfRangeException(nameof(method), method, null)
+        };
+    }
+    
+    public static async Task<Dictionary<int, T>> PerformRequest<T>(string format) where T : IFood
+    {
+        return await RetrieveByList<T>(format);
+    }
+}
+
+public enum RequestMethod
+{
+    Single = 1,
+    Multiple = 2
 }
