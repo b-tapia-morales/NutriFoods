c37a5dd08c6c40eafe4d0d1e0ce410914b9bf205
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-11-27 16:36:08 -0300

    Added minimum and maximum percentages for energy distributions

--- Utils/Nutrition/EnergyDistribution.cs
+++ Utils/Nutrition/EnergyDistribution.cs
@@ -9,30 +9,35 @@ public static class EnergyDistribution
     private const double LipidsPercentage = 0.25;
     private const double ProteinsPercentage = 0.15;
 
+    public static (double Carbohydrates, double Lipids, double Proteins) Calculate(double energy) =>
+        Calculate(energy, CarbohydratesPercentage, LipidsPercentage, ProteinsPercentage);
+
     public static (double Carbohydrates, double Lipids, double Proteins) Calculate(double energy,
-        double carbohydratesPercentage = CarbohydratesPercentage, double lipidsPercentage = LipidsPercentage,
-        double proteinsPercentage = ProteinsPercentage)
-    {
-        return ((energy / Carbohydrates.Multiplier) * carbohydratesPercentage,
-            (energy / Lipids.Multiplier) * lipidsPercentage, (energy / Proteins.Multiplier) * proteinsPercentage);
-    }
+        double carbohydratesPercentage, double lipidsPercentage, double proteinsPercentage) =>
+        ((energy / Carbohydrates.Multiplier) * carbohydratesPercentage, (energy / Lipids.Multiplier) * lipidsPercentage,
+            (energy / Proteins.Multiplier) * proteinsPercentage);
 }
 
 public sealed class Macronutrient : SmartEnum<Macronutrient>
 {
     public static readonly Macronutrient
-        Carbohydrates = new(nameof(Carbohydrates), 1, "Carbohidratos", 4);
+        Carbohydrates = new(nameof(Carbohydrates), 1, "Carbohidratos", 0.45, 0.65, 4);
 
-    public static readonly Macronutrient Lipids = new(nameof(Lipids), 2, "Lípidos", 9);
-    public static readonly Macronutrient Proteins = new(nameof(Proteins), 1, "Proteínas", 4);
-    public static readonly Macronutrient Alcohol = new(nameof(Alcohol), 1, "Alcohol", 7);
+    public static readonly Macronutrient Lipids = new(nameof(Lipids), 2, "Lípidos", 0.2, 0.35, 9);
+    public static readonly Macronutrient Proteins = new(nameof(Proteins), 1, "Proteínas", 0.1, 0.35, 4);
+    public static readonly Macronutrient Alcohol = new(nameof(Alcohol), 1, "Alcohol", null, null, 7);
 
     public string Display { get; }
+    public double? MinPercent { get; }
+    public double? MaxPercent { get; }
     public int Multiplier { get; }
 
-    private Macronutrient(string name, int value, string display, int multiplier) : base(name, value)
+    private Macronutrient(string name, int value, string display, double? minPercent, double? maxPercent,
+        int multiplier) : base(name, value)
     {
         Display = display;
+        MinPercent = minPercent;
+        MaxPercent = maxPercent;
         Multiplier = multiplier;
     }
 }
