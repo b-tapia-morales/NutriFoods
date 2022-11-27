5eb91c4fd327ad405e38c6aacacb642defb6924e
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-11-27 19:11:57 -0300

    Added default percentages to enums

--- Utils/Nutrition/EnergyDistribution.cs
+++ Utils/Nutrition/EnergyDistribution.cs
@@ -5,12 +5,9 @@ namespace Utils.Nutrition;
 
 public static class EnergyDistribution
 {
-    private const double CarbohydratesPercentage = 0.6;
-    private const double LipidsPercentage = 0.25;
-    private const double ProteinsPercentage = 0.15;
-
     public static (double Carbohydrates, double Lipids, double Proteins) Calculate(double energy) =>
-        Calculate(energy, CarbohydratesPercentage, LipidsPercentage, ProteinsPercentage);
+        Calculate(energy, Carbohydrates.DefaultPercent.GetValueOrDefault(), Lipids.DefaultPercent.GetValueOrDefault(),
+            Proteins.DefaultPercent.GetValueOrDefault());
 
     public static (double Carbohydrates, double Lipids, double Proteins) Calculate(double energy,
         double carbohydratesPercentage, double lipidsPercentage, double proteinsPercentage) =>
@@ -21,21 +18,23 @@ public static class EnergyDistribution
 public sealed class Macronutrient : SmartEnum<Macronutrient>
 {
     public static readonly Macronutrient
-        Carbohydrates = new(nameof(Carbohydrates), 1, "Carbohidratos", 0.45, 0.65, 4);
+        Carbohydrates = new(nameof(Carbohydrates), 1, "Carbohidratos", 0.6, 0.45, 0.65, 4);
 
-    public static readonly Macronutrient Lipids = new(nameof(Lipids), 2, "Lípidos", 0.2, 0.35, 9);
-    public static readonly Macronutrient Proteins = new(nameof(Proteins), 1, "Proteínas", 0.1, 0.35, 4);
-    public static readonly Macronutrient Alcohol = new(nameof(Alcohol), 1, "Alcohol", null, null, 7);
+    public static readonly Macronutrient Lipids = new(nameof(Lipids), 2, "Lípidos", 0.25, 0.2, 0.35, 9);
+    public static readonly Macronutrient Proteins = new(nameof(Proteins), 1, "Proteínas", 0.15, 0.1, 0.35, 4);
+    public static readonly Macronutrient Alcohol = new(nameof(Alcohol), 1, "Alcohol", null, null, null, 7);
 
-    public string Display { get; }
+    public string ReadableName { get; }
+    public double? DefaultPercent { get; }
     public double? MinPercent { get; }
     public double? MaxPercent { get; }
     public int Multiplier { get; }
 
-    private Macronutrient(string name, int value, string display, double? minPercent, double? maxPercent,
-        int multiplier) : base(name, value)
+    private Macronutrient(string name, int value, string readableName, double? defaultPercent, double? minPercent,
+        double? maxPercent, int multiplier) : base(name, value)
     {
-        Display = display;
+        ReadableName = readableName;
+        DefaultPercent = defaultPercent;
         MinPercent = minPercent;
         MaxPercent = maxPercent;
         Multiplier = multiplier;
