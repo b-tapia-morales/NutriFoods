008e4871f63cbc9a4cfba0ad96d14b19fe31e879
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-11-26 22:54:40 -0300

    Updated OnConfiguring() method

--- Domain/Models/NutrifoodsDbContext.cs
+++ Domain/Models/NutrifoodsDbContext.cs
@@ -66,11 +66,13 @@ public class NutrifoodsDbContext : DbContext
 
     public virtual DbSet<UserProfile> UserProfiles { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql(
-            "Host=localhost;Database=nutrifoods_db;Username=nutrifoods_dev;Password=MVmYneLqe91$",
-            x => x.UseNetTopologySuite());
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseNpgsql("Host=localhost;Database=nutrifoods_db;Username=nutrifoods_dev;Password=MVmYneLqe91$",
+            opt =>
+            {
+                opt.UseNetTopologySuite();
+                opt.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+            });
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
