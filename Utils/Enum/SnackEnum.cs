5b8c480f14dc55f1a3fe6dfee6401d11e6e32077
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2022-12-03 22:37:45 -0300

    Added enum for Snacks

---
+++ Utils/Enum/SnackEnum.cs
@@ -0,0 +1,48 @@
+using System.Collections.Immutable;
+using Ardalis.SmartEnum;
+
+namespace Utils.Enum;
+
+public class SnackEnum : SmartEnum<SnackEnum>
+{
+    public static readonly SnackEnum None =
+        new(nameof(None), (int) Snack.None, Snack.None, string.Empty);
+
+    public static readonly SnackEnum Brunch =
+        new(nameof(Brunch), (int) Snack.Brunch, Snack.Brunch, "Antes de almuerzo");
+
+    public static readonly SnackEnum Linner =
+        new(nameof(Linner), (int) Snack.Linner, Snack.Linner, "Después de almuerzo");
+
+    private static readonly IDictionary<Snack, SnackEnum> TokenDictionary =
+        new Dictionary<Snack, SnackEnum>
+        {
+            {Snack.None, None},
+            {Snack.Brunch, Brunch},
+            {Snack.Linner, Linner}
+        }.ToImmutableDictionary();
+
+    private static readonly IDictionary<string, SnackEnum> ReadableNameDictionary = TokenDictionary
+        .ToImmutableDictionary(e => e.Value.ReadableName, e => e.Value, StringComparer.InvariantCultureIgnoreCase);
+
+    public SnackEnum(string name, int value, Snack token, string readableName) : base(name, value)
+    {
+        Token = token;
+        ReadableName = readableName;
+    }
+
+    public Snack Token { get; }
+    public string ReadableName { get; }
+
+    public static SnackEnum? FromReadableName(string name) =>
+        ReadableNameDictionary.ContainsKey(name) ? ReadableNameDictionary[name] : null;
+
+    public static SnackEnum FromToken(Snack token) => TokenDictionary[token];
+}
+
+public enum Snack
+{
+    None = 0,
+    Brunch = 1,
+    Linner = 2
+}
