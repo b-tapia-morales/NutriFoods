239a1429ed97082b6c277ddff9e6a7312acad33d
Eduardo <eduardoarmando.palma@userena.cl>
2022-12-19 20:40:50 -0300

    file is added with the corresponding ranges and RecipeDistribution is updated and a new class is created to read this Amount Recipes (no me rete por los nombres de las variables)

---
+++ Utils/Averages/AmountRecipes.cs
@@ -0,0 +1,32 @@
+namespace Utils.Averages;
+
+public class AmountRecipes
+{
+    public int MealTypeValue { get; set; }
+    public ICollection<IList<(double lower, double superior)>> RangesAmount { get; set; } = null!;
+
+    public static IEnumerable<AmountRecipes> GetAmount(string path)
+    {
+        var file = File.ReadAllLines(path).Select(x => x.Split(";"));
+        var amountRecipes = new List<AmountRecipes>();
+        foreach (var line in file)
+        {
+            var rangesAmount = new List<IList<(double lower, double superior)>>();
+            for (var i = 1; i < line.Length; i++)
+            {
+                var rangesList = line[i].Split(":");
+                var ranges = rangesList.Select(values => values.Split(","))
+                    .Select(value => (double.Parse(value[0]), double.Parse(value[1]))).ToList();
+                rangesAmount.Add(ranges);
+            }
+            var amountRecipe = new AmountRecipes
+            {
+                MealTypeValue = int.Parse(line[0]),
+                RangesAmount = rangesAmount,
+            };
+            amountRecipes.Add(amountRecipe);
+        }
+
+        return amountRecipes;
+    }
+}
