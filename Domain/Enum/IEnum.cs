c74114027b8a089b682ce5550419dfc937d3665d
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-11-03 19:35:44 -0300

    Fixed minor bug where enums didn't preserve order

--- Domain/Enum/IEnum.cs
+++ Domain/Enum/IEnum.cs
@@ -1,28 +1,30 @@
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using Ardalis.SmartEnum;
+using static System.StringComparer;
 
 namespace Domain.Enum;
 
 [SuppressMessage("ReSharper", "ReturnTypeCanBeEnumerable.Global")]
 public interface IEnum<out T, TEnum>
     where T : SmartEnum<T>, IEnum<T, TEnum>
-    where TEnum : struct, System.Enum
+    where TEnum : struct, System.Enum, IConvertible
 {
-    private static readonly IReadOnlyCollection<TEnum> Tokens = System.Enum.GetValues<TEnum>().ToImmutableList();
+    private static readonly IReadOnlyCollection<TEnum> Tokens =
+        System.Enum.GetValues<TEnum>().OrderBy(e => e.ToInt32(null)).ToImmutableList();
 
     string ReadableName { get; }
 
-    static IReadOnlyCollection<T> Values() => SmartEnum<T>.List;
+    static IReadOnlyCollection<T> Values() => SmartEnum<T>.List.OrderBy(e => e.Value).ToImmutableList();
 
     static IReadOnlyCollection<T> NonNullValues() => Values().Skip(1).ToImmutableList();
 
     static IReadOnlyDictionary<TEnum, T> TokenDictionary() =>
-        Tokens.Zip(Values(), (k, v) => new { Key = k, Value = v }).ToImmutableDictionary(e => e.Key, e => e.Value);
+        Tokens.Zip(Values(), (k, v) => new { Key = k, Value = v })
+            .ToImmutableSortedDictionary(e => e.Key, e => e.Value);
 
     static IReadOnlyDictionary<string, T> ReadableNameDictionary() =>
-        TokenDictionary().ToImmutableDictionary(e => e.Value.ReadableName, e => e.Value,
-            StringComparer.InvariantCultureIgnoreCase);
+        TokenDictionary().ToImmutableDictionary(e => e.Value.ReadableName, e => e.Value, InvariantCultureIgnoreCase);
 
     static T FromToken(TEnum token) =>
         TokenDictionary().TryGetValue(token, out var value) ? value : throw new KeyNotFoundException();
