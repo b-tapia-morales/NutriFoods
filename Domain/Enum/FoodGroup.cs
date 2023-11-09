fa802daf1bc0a5dc04339a32e8affdc155a46041
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-11-09 20:27:07 -0300

    Modified hierarchical enums
    Reworked constructor values to define if a category is a super category based on its parent category
    Added saturated and monosaturated fatty acid enums

--- Domain/Enum/FoodGroup.cs
+++ Domain/Enum/FoodGroup.cs
@@ -5,133 +5,128 @@ namespace Domain.Enum;
 public class FoodGroup : SmartEnum<FoodGroup>, IHierarchicalEnum<FoodGroup, FoodGroupToken>
 {
     public static readonly FoodGroup None =
-        new(nameof(None), (int)FoodGroupToken.None, "", false, null);
+        new(nameof(None), (int)FoodGroupToken.None, "", null);
 
     public static readonly FoodGroup Fruits =
-        new(nameof(Fruits), (int)FoodGroupToken.Fruits, "Frutas", true, null);
+        new(nameof(Fruits), (int)FoodGroupToken.Fruits, "Frutas", null);
 
     public static readonly FoodGroup Vegetables =
-        new(nameof(Vegetables), (int)FoodGroupToken.Vegetables, "Vegetales", true, null);
+        new(nameof(Vegetables), (int)FoodGroupToken.Vegetables, "Vegetales", null);
 
     public static readonly FoodGroup Grains =
-        new(nameof(Grains), (int)FoodGroupToken.Grains, "Granos", true, null);
+        new(nameof(Grains), (int)FoodGroupToken.Grains, "Granos", null);
 
     public static readonly FoodGroup ProteinFoods =
-        new(nameof(ProteinFoods), (int)FoodGroupToken.ProteinFoods, "Alimentos Proteicos", true, null);
+        new(nameof(ProteinFoods), (int)FoodGroupToken.ProteinFoods, "Alimentos Proteicos", null);
 
     public static readonly FoodGroup Dairy =
-        new(nameof(Dairy), (int)FoodGroupToken.Dairy, "Lácteos", true, null);
+        new(nameof(Dairy), (int)FoodGroupToken.Dairy, "Lácteos", null);
 
     public static readonly FoodGroup Other =
-        new(nameof(Other), (int)FoodGroupToken.Other, "Otros", true, null);
+        new(nameof(Other), (int)FoodGroupToken.Other, "Otros", null);
 
     public static readonly FoodGroup WholeFruit =
-        new(nameof(WholeFruit), (int)FoodGroupToken.WholeFruit, "Fruta Entera", false, Fruits);
+        new(nameof(WholeFruit), (int)FoodGroupToken.WholeFruit, "Fruta Entera", Fruits);
 
     public static readonly FoodGroup FruitJuice =
-        new(nameof(FruitJuice), (int)FoodGroupToken.FruitJuice, "Jugo de Fruta", false, Fruits);
+        new(nameof(FruitJuice), (int)FoodGroupToken.FruitJuice, "Jugo de Fruta", Fruits);
 
     public static readonly FoodGroup DarkGreenVegetables =
-        new(nameof(DarkGreenVegetables), (int)FoodGroupToken.DarkGreenVegetables, "Vegetales verdes", false,
-            Vegetables);
+        new(nameof(DarkGreenVegetables), (int)FoodGroupToken.DarkGreenVegetables, "Vegetales verdes", Vegetables);
 
     public static readonly FoodGroup RedAndOrangeVegetables =
-        new(nameof(RedAndOrangeVegetables), (int)FoodGroupToken.RedAndOrangeVegetables, "Vegetales rojos", false,
-            Vegetables);
+        new(nameof(RedAndOrangeVegetables), (int)FoodGroupToken.RedAndOrangeVegetables, "Vegetales rojos", Vegetables);
 
     public static readonly FoodGroup Legumes =
-        new(nameof(Legumes), (int)FoodGroupToken.Legumes, "Legumbres", false, Vegetables);
+        new(nameof(Legumes), (int)FoodGroupToken.Legumes, "Legumbres", Vegetables);
 
     public static readonly FoodGroup StarchyVegetables =
-        new(nameof(StarchyVegetables), (int)FoodGroupToken.StarchyVegetables, "Vegetales con almidón", false,
-            Vegetables);
+        new(nameof(StarchyVegetables), (int)FoodGroupToken.StarchyVegetables, "Vegetales con almidón", Vegetables);
 
     public static readonly FoodGroup OtherVegetables =
-        new(nameof(OtherVegetables), (int)FoodGroupToken.OtherVegetables, "Otros Vegetales", false, Vegetables);
+        new(nameof(OtherVegetables), (int)FoodGroupToken.OtherVegetables, "Otros Vegetales", Vegetables);
 
     public static readonly FoodGroup WholeGrains =
-        new(nameof(WholeGrains), (int)FoodGroupToken.WholeGrains, "Granos Enteros", false, Grains);
+        new(nameof(WholeGrains), (int)FoodGroupToken.WholeGrains, "Granos Enteros", Grains);
 
     public static readonly FoodGroup RefinedGrains =
-        new(nameof(RefinedGrains), (int)FoodGroupToken.RefinedGrains, "Granos Refinados", false, Grains);
+        new(nameof(RefinedGrains), (int)FoodGroupToken.RefinedGrains, "Granos Refinados", Grains);
 
     public static readonly FoodGroup Seafood =
-        new(nameof(Seafood), (int)FoodGroupToken.Seafood, "Pescados y Mariscos", false, ProteinFoods);
+        new(nameof(Seafood), (int)FoodGroupToken.Seafood, "Pescados y Mariscos", ProteinFoods);
 
     public static readonly FoodGroup MeatPoultryAndEggs =
-        new(nameof(MeatPoultryAndEggs), (int)FoodGroupToken.MeatPoultryAndEggs, "Carnes, Aves y Huevos", false,
-            ProteinFoods);
+        new(nameof(MeatPoultryAndEggs), (int)FoodGroupToken.MeatPoultryAndEggs, "Carnes, Aves y Huevos", ProteinFoods);
 
     public static readonly FoodGroup NutsSeedsAndSoy =
-        new(nameof(NutsSeedsAndSoy), (int)FoodGroupToken.NutsSeedsAndSoy, "Frutos secos, Semillas y Soya", false,
+        new(nameof(NutsSeedsAndSoy), (int)FoodGroupToken.NutsSeedsAndSoy, "Frutos secos, Semillas y Soya",
             ProteinFoods);
 
     public static readonly FoodGroup MilkAndYogurt =
-        new(nameof(MilkAndYogurt), (int)FoodGroupToken.MilkAndYogurt, "Leche y Yogurt", false, Dairy);
+        new(nameof(MilkAndYogurt), (int)FoodGroupToken.MilkAndYogurt, "Leche y Yogurt", Dairy);
 
     public static readonly FoodGroup Sugars =
-        new(nameof(Sugars), (int)FoodGroupToken.Sugars, "Azúcares", false, Other);
+        new(nameof(Sugars), (int)FoodGroupToken.Sugars, "Azúcares", Other);
 
     public static readonly FoodGroup Salts =
-        new(nameof(Salts), (int)FoodGroupToken.Salts, "Sales", false, Other);
+        new(nameof(Salts), (int)FoodGroupToken.Salts, "Sales", Other);
 
     public static readonly FoodGroup FatsAndOils =
-        new(nameof(FatsAndOils), (int)FoodGroupToken.FatsAndOils, "Aceites y grasas", false, Other);
+        new(nameof(FatsAndOils), (int)FoodGroupToken.FatsAndOils, "Aceites y grasas", Other);
 
     public static readonly FoodGroup DressingsAndSauces =
-        new(nameof(DressingsAndSauces), (int)FoodGroupToken.DressingsAndSauces, "Aderezos y salsas", false, Other);
+        new(nameof(DressingsAndSauces), (int)FoodGroupToken.DressingsAndSauces, "Aderezos y salsas", Other);
 
     public static readonly FoodGroup SeasoningsAndSpices =
-        new(nameof(SeasoningsAndSpices), (int)FoodGroupToken.SeasoningsAndSpices, "Condimentos y especias", false,
-            Other);
+        new(nameof(SeasoningsAndSpices), (int)FoodGroupToken.SeasoningsAndSpices, "Condimentos y especias", Other);
 
     public static readonly FoodGroup Algae =
-        new(nameof(Algae), (int)FoodGroupToken.Algae, "Algas", false, Other);
+        new(nameof(Algae), (int)FoodGroupToken.Algae, "Algas", Other);
 
     public static readonly FoodGroup Cheese =
-        new(nameof(Cheese), (int)FoodGroupToken.Cheese, "Queso", false, Dairy);
+        new(nameof(Cheese), (int)FoodGroupToken.Cheese, "Queso", Dairy);
 
     public static readonly FoodGroup Fish =
-        new(nameof(Fish), (int)FoodGroupToken.Fish, "Pescados", false, Seafood);
+        new(nameof(Fish), (int)FoodGroupToken.Fish, "Pescados", Seafood);
 
     public static readonly FoodGroup Shellfish =
-        new(nameof(Shellfish), (int)FoodGroupToken.Shellfish, "Mariscos", false, Seafood);
+        new(nameof(Shellfish), (int)FoodGroupToken.Shellfish, "Mariscos", Seafood);
 
     public static readonly FoodGroup Meat =
-        new(nameof(Meat), (int)FoodGroupToken.Meat, "Carnes", false, MeatPoultryAndEggs);
+        new(nameof(Meat), (int)FoodGroupToken.Meat, "Carnes", MeatPoultryAndEggs);
 
     public static readonly FoodGroup Poultry =
-        new(nameof(Poultry), (int)FoodGroupToken.Poultry, "Aves", false, MeatPoultryAndEggs);
+        new(nameof(Poultry), (int)FoodGroupToken.Poultry, "Aves", MeatPoultryAndEggs);
 
     public static readonly FoodGroup Eggs =
-        new(nameof(Eggs), (int)FoodGroupToken.Eggs, "Huevos", false, MeatPoultryAndEggs);
+        new(nameof(Eggs), (int)FoodGroupToken.Eggs, "Huevos", MeatPoultryAndEggs);
 
     public static readonly FoodGroup Nuts =
-        new(nameof(Nuts), (int)FoodGroupToken.Nuts, "Frutos secos", false, NutsSeedsAndSoy);
+        new(nameof(Nuts), (int)FoodGroupToken.Nuts, "Frutos secos", NutsSeedsAndSoy);
 
     public static readonly FoodGroup Seeds =
-        new(nameof(Seeds), (int)FoodGroupToken.Seeds, "Semillas", false, NutsSeedsAndSoy);
+        new(nameof(Seeds), (int)FoodGroupToken.Seeds, "Semillas", NutsSeedsAndSoy);
 
     public static readonly FoodGroup Soy =
-        new(nameof(Soy), (int)FoodGroupToken.Soy, "Soya", false, NutsSeedsAndSoy);
+        new(nameof(Soy), (int)FoodGroupToken.Soy, "Soya", NutsSeedsAndSoy);
 
     public static readonly FoodGroup Milk =
-        new(nameof(Milk), (int)FoodGroupToken.Milk, "Leche", false, MilkAndYogurt);
+        new(nameof(Milk), (int)FoodGroupToken.Milk, "Leche", MilkAndYogurt);
 
     public static readonly FoodGroup Yogurt =
-        new(nameof(Yogurt), (int)FoodGroupToken.Yogurt, "Yogurt", false, MilkAndYogurt);
+        new(nameof(Yogurt), (int)FoodGroupToken.Yogurt, "Yogurt", MilkAndYogurt);
 
     public static readonly FoodGroup Sweets =
-        new(nameof(Sweets), (int)FoodGroupToken.Sweets, "Dulces", false, Sugars);
+        new(nameof(Sweets), (int)FoodGroupToken.Sweets, "Dulces", Sugars);
 
     public static readonly FoodGroup SaltySnacks =
-        new(nameof(SaltySnacks), (int)FoodGroupToken.SaltySnacks, "Bocadillos Salados", false, Salts);
+        new(nameof(SaltySnacks), (int)FoodGroupToken.SaltySnacks, "Bocadillos Salados", Salts);
 
-    private FoodGroup(string name, int value, string readableName, bool isTopCategory, FoodGroup? category) :
+    private FoodGroup(string name, int value, string readableName, FoodGroup? category) :
         base(name, value)
     {
         ReadableName = readableName;
-        IsTopCategory = isTopCategory;
+        IsTopCategory = category == null;
         Category = category;
     }
 
--- Domain/Enum/Nutrient.cs
+++ Domain/Enum/Nutrient.cs
@@ -8,20 +8,16 @@ public class Nutrient : SmartEnum<Nutrient>, IHierarchicalEnum<Nutrient, Nutrien
         new(nameof(None), (int)NutrientToken.None, "", "", false, null);
 
     public static readonly Nutrient Energy =
-        new(nameof(Energy), (int)NutrientToken.Energy, "Energía", "", true, null);
+        new(nameof(Energy), (int)NutrientToken.Energy, "Energía, total", "", true, null);
 
     public static readonly Nutrient Carbohydrates =
-        new(nameof(Carbohydrates), (int)NutrientToken.Carbohydrates, "Carbohidratos", "", true, null);
-
-    public static readonly Nutrient CarbohydratesTotal =
-        new(nameof(CarbohydratesTotal), (int)NutrientToken.CarbohydratesTotal, "Carbohidratos, Total", "", false,
-            Carbohydrates);
+        new(nameof(Carbohydrates), (int)NutrientToken.Carbohydrates, "Carbohidratos, total", "", true, null);
 
     public static readonly Nutrient Fiber =
-        new(nameof(Fiber), (int)NutrientToken.Fiber, "Fibra", "", false, CarbohydratesTotal);
+        new(nameof(Fiber), (int)NutrientToken.Fiber, "Fibra", "", false, Carbohydrates);
 
     public static readonly Nutrient Sugars =
-        new(nameof(Sugars), (int)NutrientToken.Sugars, "Azúcares", "", false, CarbohydratesTotal);
+        new(nameof(Sugars), (int)NutrientToken.Sugars, "Azúcares", "", true, Carbohydrates);
 
     public static readonly Nutrient Sucrose =
         new(nameof(Sucrose), (int)NutrientToken.Sucrose, "Sucrosa", "Sacarosa", false, Sugars);
@@ -42,10 +38,125 @@ public class Nutrient : SmartEnum<Nutrient>, IHierarchicalEnum<Nutrient, Nutrien
         new(nameof(Galactose), (int)NutrientToken.Galactose, "Galactosa", "", false, Sugars);
 
     public static readonly Nutrient Starch =
-        new(nameof(Starch), (int)NutrientToken.Starch, "Almidón", "Fécula", false, CarbohydratesTotal);
+        new(nameof(Starch), (int)NutrientToken.Starch, "Almidón", "Fécula", false, Carbohydrates);
+
+    public static readonly Nutrient FattyAcids =
+        new(nameof(FattyAcids), (int)NutrientToken.FattyAcids, "Ácidos grasos, total", "", true, null);
+
+    public static readonly Nutrient SaturatedFattyAcids =
+        new(nameof(SaturatedFattyAcids), (int)NutrientToken.SaturatedFattyAcids, "Ácidos grasos saturados, total", "",
+            true, FattyAcids);
+
+    public static readonly Nutrient ButanoicAcid =
+        new(nameof(ButanoicAcid), (int)NutrientToken.ButanoicAcid, "Ácido butanoico", "Ácido butírico", false,
+            SaturatedFattyAcids);
+
+    public static readonly Nutrient HexanoicAcid =
+        new(nameof(HexanoicAcid), (int)NutrientToken.HexanoicAcid, "Ácido hexanoico", "Ácido caproico", false,
+            SaturatedFattyAcids);
+
+    public static readonly Nutrient OctanoicAcid =
+        new(nameof(OctanoicAcid), (int)NutrientToken.OctanoicAcid, "Ácido octanoico", "Ácido caprílico", false,
+            SaturatedFattyAcids);
+
+    public static readonly Nutrient DecanoicAcid =
+        new(nameof(DecanoicAcid), (int)NutrientToken.DecanoicAcid, "Ácido decanoico", "Ácido cáprico", false,
+            SaturatedFattyAcids);
+
+    public static readonly Nutrient DodecanoicAcid =
+        new(nameof(DodecanoicAcid), (int)NutrientToken.DodecanoicAcid, "Ácido dodecanoico", "Ácido láurico", false,
+            SaturatedFattyAcids);
+
+    public static readonly Nutrient TridecanoicAcid =
+        new(nameof(TridecanoicAcid), (int)NutrientToken.TridecanoicAcid, "Ácido tridecanoico", "", false,
+            SaturatedFattyAcids);
+
+    public static readonly Nutrient TetradecanoicAcid =
+        new(nameof(TetradecanoicAcid), (int)NutrientToken.TetradecanoicAcid, "Ácido tetradecanoico", "Ácido mirístico",
+            false, SaturatedFattyAcids);
+
+    public static readonly Nutrient PentadecanoicAcid =
+        new(nameof(PentadecanoicAcid), (int)NutrientToken.PentadecanoicAcid, "Ácido pentadecanoico", "", false,
+            SaturatedFattyAcids);
+
+    public static readonly Nutrient HexadecanoicAcid =
+        new(nameof(HexadecanoicAcid), (int)NutrientToken.HexadecanoicAcid, "Ácido hexadecanoico", "Ácido palmítico",
+            false, SaturatedFattyAcids);
+
+    public static readonly Nutrient HeptadecanoicAcid =
+        new(nameof(HeptadecanoicAcid), (int)NutrientToken.HeptadecanoicAcid, "Ácido heptadecanoico", "Ácido margárico",
+            false, SaturatedFattyAcids);
+
+    public static readonly Nutrient OctadecanoicAcid =
+        new(nameof(OctadecanoicAcid), (int)NutrientToken.OctadecanoicAcid, "Ácido octadecanoico", "Ácido esteárico",
+            false, SaturatedFattyAcids);
+
+    public static readonly Nutrient EicosanoicAcid =
+        new(nameof(EicosanoicAcid), (int)NutrientToken.EicosanoicAcid, "Ácido eicosanoico", "Ácido araquídico", false,
+            SaturatedFattyAcids);
+
+    public static readonly Nutrient DocosanoicAcid =
+        new(nameof(DocosanoicAcid), (int)NutrientToken.DocosanoicAcid, "Ácido docosanoico", "Ácido behénico", false,
+            SaturatedFattyAcids);
+
+    public static readonly Nutrient TetracosanoicAcid =
+        new(nameof(TetracosanoicAcid), (int)NutrientToken.TetracosanoicAcid, "Ácido tetracosanoico",
+            "Ácido lignocérico", false, SaturatedFattyAcids);
+
+    public static readonly Nutrient MonounsaturatedFattyAcids =
+        new(nameof(MonounsaturatedFattyAcids), (int)NutrientToken.MonounsaturatedFattyAcids,
+            "Ácidos grasos monoinsaturados, total", "", true, FattyAcids);
+
+    public static readonly Nutrient TetradecenoicAcid =
+        new(nameof(TetradecenoicAcid), (int)NutrientToken.TetradecenoicAcid, "Ácido tetradecenoico",
+            "Ácido miristoleico", false, MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient PentadecenoicAcid =
+        new(nameof(PentadecenoicAcid), (int)NutrientToken.PentadecenoicAcid, "Ácido pentadecenoico", "", false,
+            MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient HexadecenoicAcid =
+        new(nameof(HexadecenoicAcid), (int)NutrientToken.HexadecenoicAcid, "Ácido hexadecenoico", "Ácido palmitoleico",
+            false, MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient CisHexadecenoicAcid =
+        new(nameof(CisHexadecenoicAcid), (int)NutrientToken.CisHexadecenoicAcid, "Ácido cis-hexadecenoico", "", false,
+            MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient HeptadecenoicAcid =
+        new(nameof(HeptadecenoicAcid), (int)NutrientToken.HeptadecenoicAcid, "Ácido heptadecenoico", "", false,
+            MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient IheptadecenoicAcid =
+        new(nameof(IheptadecenoicAcid), (int)NutrientToken.IheptadecenoicAcid, "Ácido i-heptadecenoico", "", false,
+            MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient OctadecenoicAcid =
+        new(nameof(OctadecenoicAcid), (int)NutrientToken.OctadecenoicAcid, "Ácido octadecenoico", "Ácido oleico", false,
+            MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient CisOctadecenoicAcid =
+        new(nameof(CisOctadecenoicAcid), (int)NutrientToken.CisOctadecenoicAcid, "Ácido cis-octadecenoico", "", false,
+            MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient EicosenoicAcid =
+        new(nameof(EicosenoicAcid), (int)NutrientToken.EicosenoicAcid, "Ácido eicosenoico", "Ácido gadoleico", false,
+            MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient DocosenoicAcid =
+        new(nameof(DocosenoicAcid), (int)NutrientToken.DocosenoicAcid, "Ácido docosenoico", "", false,
+            MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient CisDocosenoicAcid =
+        new(nameof(CisDocosenoicAcid), (int)NutrientToken.CisDocosenoicAcid, "Ácido cis-docosenoico", "Ácido erúcico",
+            false, MonounsaturatedFattyAcids);
+
+    public static readonly Nutrient CisTetracosenoicAcid =
+        new(nameof(CisTetracosenoicAcid), (int)NutrientToken.CisTetracosenoicAcid, "Ácido cis-tetracosenoico",
+            "Ácido nervónico", false, MonounsaturatedFattyAcids);
 
     public static readonly Nutrient Minerals =
-        new(nameof(Minerals), (int)NutrientToken.Minerals, "Minerales", "", true, null);
+        new(nameof(Minerals), (int)NutrientToken.Minerals, "Minerales", "", false, null);
 
     public static readonly Nutrient Calcium =
         new(nameof(Calcium), (int)NutrientToken.Calcium, "Calcio", "Ca", false, Minerals);
@@ -80,18 +191,20 @@ public class Nutrient : SmartEnum<Nutrient>, IHierarchicalEnum<Nutrient, Nutrien
     public static readonly Nutrient Fluoride =
         new(nameof(Fluoride), (int)NutrientToken.Fluoride, "Fluoruro", "F", false, Minerals);
 
-    private Nutrient(string name, int value, string readableName, string optionalName,
-        bool isTopCategory, Nutrient? category) : base(name, value)
+    private Nutrient(string name, int value, string readableName, string optionalName, bool isTotal,
+        Nutrient? category) : base(name, value)
     {
         ReadableName = readableName;
         OptionalName = optionalName;
-        IsTopCategory = isTopCategory;
+        IsTopCategory = category == null;
+        IsTotal = isTotal;
         Category = category;
     }
 
     public string ReadableName { get; }
     public string OptionalName { get; }
     public bool IsTopCategory { get; }
+    public bool IsTotal { get; }
     public Nutrient? Category { get; }
 }
 
@@ -100,7 +213,6 @@ public enum NutrientToken
     None,
     Energy,
     Carbohydrates,
-    CarbohydratesTotal,
     Fiber,
     Sugars,
     Sucrose,
@@ -110,6 +222,22 @@ public enum NutrientToken
     Maltose,
     Galactose,
     Starch,
+    FattyAcids,
+    SaturatedFattyAcids,
+    ButanoicAcid,
+    HexanoicAcid,
+    OctanoicAcid,
+    DecanoicAcid,
+    DodecanoicAcid,
+    TridecanoicAcid,
+    TetradecanoicAcid,
+    PentadecanoicAcid,
+    HexadecanoicAcid,
+    HeptadecanoicAcid,
+    OctadecanoicAcid,
+    EicosanoicAcid,
+    DocosanoicAcid,
+    TetracosanoicAcid,
     Minerals,
     Calcium,
     Iron,
@@ -121,5 +249,18 @@ public enum NutrientToken
     Copper,
     Manganese,
     Selenium,
-    Fluoride
+    Fluoride,
+    MonounsaturatedFattyAcids,
+    TetradecenoicAcid,
+    PentadecenoicAcid,
+    HexadecenoicAcid,
+    CisHexadecenoicAcid,
+    HeptadecenoicAcid,
+    IheptadecenoicAcid,
+    OctadecenoicAcid,
+    CisOctadecenoicAcid,
+    EicosenoicAcid,
+    DocosenoicAcid,
+    CisDocosenoicAcid,
+    CisTetracosenoicAcid
 }
