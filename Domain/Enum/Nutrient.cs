8e7a225e6a4a713a09928a5b47d56428bb97ce7f
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-11-09 23:11:59 -0300

    Completed adding all fatty acids

--- Domain/Enum/Nutrient.cs
+++ Domain/Enum/Nutrient.cs
@@ -43,117 +43,192 @@ public class Nutrient : SmartEnum<Nutrient>, IHierarchicalEnum<Nutrient, Nutrien
     public static readonly Nutrient FattyAcids =
         new(nameof(FattyAcids), (int)NutrientToken.FattyAcids, "Ácidos grasos, total", "", true, null);
 
-    public static readonly Nutrient SaturatedFattyAcids =
-        new(nameof(SaturatedFattyAcids), (int)NutrientToken.SaturatedFattyAcids, "Ácidos grasos saturados, total", "",
+    public static readonly Nutrient Saturated =
+        new(nameof(Saturated), (int)NutrientToken.Saturated, "Ácidos grasos saturados, total", "", true, FattyAcids);
+
+    public static readonly Nutrient Butanoic =
+        new(nameof(Butanoic), (int)NutrientToken.Butanoic, "Ácido butanoico", "Ácido butírico", false, Saturated);
+
+    public static readonly Nutrient Hexanoic =
+        new(nameof(Hexanoic), (int)NutrientToken.Hexanoic, "Ácido hexanoico", "Ácido caproico", false, Saturated);
+
+    public static readonly Nutrient Octanoic =
+        new(nameof(Octanoic), (int)NutrientToken.Octanoic, "Ácido octanoico", "Ácido caprílico", false, Saturated);
+
+    public static readonly Nutrient Decanoic =
+        new(nameof(Decanoic), (int)NutrientToken.Decanoic, "Ácido decanoico", "Ácido cáprico", false, Saturated);
+
+    public static readonly Nutrient Dodecanoic =
+        new(nameof(Dodecanoic), (int)NutrientToken.Dodecanoic, "Ácido dodecanoico", "Ácido láurico", false, Saturated);
+
+    public static readonly Nutrient Tridecanoic =
+        new(nameof(Tridecanoic), (int)NutrientToken.Tridecanoic, "Ácido tridecanoico", "", false, Saturated);
+
+    public static readonly Nutrient Tetradecanoic =
+        new(nameof(Tetradecanoic), (int)NutrientToken.Tetradecanoic, "Ácido tetradecanoico", "Ácido mirístico", false,
+            Saturated);
+
+    public static readonly Nutrient Pentadecanoic =
+        new(nameof(Pentadecanoic), (int)NutrientToken.Pentadecanoic, "Ácido pentadecanoico", "", false, Saturated);
+
+    public static readonly Nutrient Hexadecanoic =
+        new(nameof(Hexadecanoic), (int)NutrientToken.Hexadecanoic, "Ácido hexadecanoico", "Ácido palmítico", false,
+            Saturated);
+
+    public static readonly Nutrient Heptadecanoic =
+        new(nameof(Heptadecanoic), (int)NutrientToken.Heptadecanoic, "Ácido heptadecanoico", "Ácido margárico", false,
+            Saturated);
+
+    public static readonly Nutrient Octadecanoic =
+        new(nameof(Octadecanoic), (int)NutrientToken.Octadecanoic, "Ácido octadecanoico", "Ácido esteárico", false,
+            Saturated);
+
+    public static readonly Nutrient Eicosanoic =
+        new(nameof(Eicosanoic), (int)NutrientToken.Eicosanoic, "Ácido eicosanoico", "Ácido araquídico", false,
+            Saturated);
+
+    public static readonly Nutrient Docosanoic =
+        new(nameof(Docosanoic), (int)NutrientToken.Docosanoic, "Ácido docosanoico", "Ácido behénico", false, Saturated);
+
+    public static readonly Nutrient Tetracosanoic =
+        new(nameof(Tetracosanoic), (int)NutrientToken.Tetracosanoic, "Ácido tetracosanoico", "Ácido lignocérico", false,
+            Saturated);
+
+    public static readonly Nutrient Monounsaturated =
+        new(nameof(Monounsaturated), (int)NutrientToken.Monounsaturated, "Ácidos grasos monoinsaturados, total", "",
             true, FattyAcids);
 
-    public static readonly Nutrient ButanoicAcid =
-        new(nameof(ButanoicAcid), (int)NutrientToken.ButanoicAcid, "Ácido butanoico", "Ácido butírico", false,
-            SaturatedFattyAcids);
+    public static readonly Nutrient Tetradecenoic =
+        new(nameof(Tetradecenoic), (int)NutrientToken.Tetradecenoic, "Ácido tetradecenoico", "Ácido miristoleico",
+            false, Monounsaturated);
 
-    public static readonly Nutrient HexanoicAcid =
-        new(nameof(HexanoicAcid), (int)NutrientToken.HexanoicAcid, "Ácido hexanoico", "Ácido caproico", false,
-            SaturatedFattyAcids);
+    public static readonly Nutrient Pentadecenoic =
+        new(nameof(Pentadecenoic), (int)NutrientToken.Pentadecenoic, "Ácido pentadecenoico", "", false,
+            Monounsaturated);
 
-    public static readonly Nutrient OctanoicAcid =
-        new(nameof(OctanoicAcid), (int)NutrientToken.OctanoicAcid, "Ácido octanoico", "Ácido caprílico", false,
-            SaturatedFattyAcids);
+    public static readonly Nutrient Hexadecenoic =
+        new(nameof(Hexadecenoic), (int)NutrientToken.Hexadecenoic, "Ácido hexadecenoico", "Ácido palmitoleico", false,
+            Monounsaturated);
 
-    public static readonly Nutrient DecanoicAcid =
-        new(nameof(DecanoicAcid), (int)NutrientToken.DecanoicAcid, "Ácido decanoico", "Ácido cáprico", false,
-            SaturatedFattyAcids);
+    public static readonly Nutrient CisHexadecenoic =
+        new(nameof(CisHexadecenoic), (int)NutrientToken.CisHexadecenoic, "Ácido cis-hexadecenoico", "", false,
+            Monounsaturated);
 
-    public static readonly Nutrient DodecanoicAcid =
-        new(nameof(DodecanoicAcid), (int)NutrientToken.DodecanoicAcid, "Ácido dodecanoico", "Ácido láurico", false,
-            SaturatedFattyAcids);
+    public static readonly Nutrient Heptadecenoic =
+        new(nameof(Heptadecenoic), (int)NutrientToken.Heptadecenoic, "Ácido heptadecenoico", "", false,
+            Monounsaturated);
 
-    public static readonly Nutrient TridecanoicAcid =
-        new(nameof(TridecanoicAcid), (int)NutrientToken.TridecanoicAcid, "Ácido tridecanoico", "", false,
-            SaturatedFattyAcids);
+    public static readonly Nutrient Iheptadecenoic =
+        new(nameof(Iheptadecenoic), (int)NutrientToken.Iheptadecenoic, "Ácido i-heptadecenoico", "", false,
+            Monounsaturated);
 
-    public static readonly Nutrient TetradecanoicAcid =
-        new(nameof(TetradecanoicAcid), (int)NutrientToken.TetradecanoicAcid, "Ácido tetradecanoico", "Ácido mirístico",
-            false, SaturatedFattyAcids);
+    public static readonly Nutrient Octadecenoic =
+        new(nameof(Octadecenoic), (int)NutrientToken.Octadecenoic, "Ácido octadecenoico", "Ácido oleico", false,
+            Monounsaturated);
 
-    public static readonly Nutrient PentadecanoicAcid =
-        new(nameof(PentadecanoicAcid), (int)NutrientToken.PentadecanoicAcid, "Ácido pentadecanoico", "", false,
-            SaturatedFattyAcids);
+    public static readonly Nutrient CisOctadecenoic =
+        new(nameof(CisOctadecenoic), (int)NutrientToken.CisOctadecenoic, "Ácido cis-octadecenoico", "", false,
+            Monounsaturated);
 
-    public static readonly Nutrient HexadecanoicAcid =
-        new(nameof(HexadecanoicAcid), (int)NutrientToken.HexadecanoicAcid, "Ácido hexadecanoico", "Ácido palmítico",
-            false, SaturatedFattyAcids);
+    public static readonly Nutrient Eicosenoic =
+        new(nameof(Eicosenoic), (int)NutrientToken.Eicosenoic, "Ácido eicosenoico", "Ácido gadoleico", false,
+            Monounsaturated);
 
-    public static readonly Nutrient HeptadecanoicAcid =
-        new(nameof(HeptadecanoicAcid), (int)NutrientToken.HeptadecanoicAcid, "Ácido heptadecanoico", "Ácido margárico",
-            false, SaturatedFattyAcids);
+    public static readonly Nutrient Docosenoic =
+        new(nameof(Docosenoic), (int)NutrientToken.Docosenoic, "Ácido docosenoico", "", false, Monounsaturated);
 
-    public static readonly Nutrient OctadecanoicAcid =
-        new(nameof(OctadecanoicAcid), (int)NutrientToken.OctadecanoicAcid, "Ácido octadecanoico", "Ácido esteárico",
-            false, SaturatedFattyAcids);
+    public static readonly Nutrient CisDocosenoic =
+        new(nameof(CisDocosenoic), (int)NutrientToken.CisDocosenoic, "Ácido cis-docosenoico", "Ácido erúcico", false,
+            Monounsaturated);
 
-    public static readonly Nutrient EicosanoicAcid =
-        new(nameof(EicosanoicAcid), (int)NutrientToken.EicosanoicAcid, "Ácido eicosanoico", "Ácido araquídico", false,
-            SaturatedFattyAcids);
+    public static readonly Nutrient CisTetracosenoic =
+        new(nameof(CisTetracosenoic), (int)NutrientToken.CisTetracosenoic, "Ácido cis-tetracosenoico",
+            "Ácido nervónico", false, Monounsaturated);
 
-    public static readonly Nutrient DocosanoicAcid =
-        new(nameof(DocosanoicAcid), (int)NutrientToken.DocosanoicAcid, "Ácido docosanoico", "Ácido behénico", false,
-            SaturatedFattyAcids);
+    public static readonly Nutrient Polyunsaturated =
+        new(nameof(Polyunsaturated), (int)NutrientToken.Polyunsaturated, "Ácidos grasos poliinsaturados, total", "",
+            true, FattyAcids);
 
-    public static readonly Nutrient TetracosanoicAcid =
-        new(nameof(TetracosanoicAcid), (int)NutrientToken.TetracosanoicAcid, "Ácido tetracosanoico",
-            "Ácido lignocérico", false, SaturatedFattyAcids);
+    public static readonly Nutrient Octadecadienoic =
+        new(nameof(Octadecadienoic), (int)NutrientToken.Octadecadienoic, "Ácido octadecadienoico", "Ácido linoleico",
+            false, Polyunsaturated);
 
-    public static readonly Nutrient MonounsaturatedFattyAcids =
-        new(nameof(MonounsaturatedFattyAcids), (int)NutrientToken.MonounsaturatedFattyAcids,
-            "Ácidos grasos monoinsaturados, total", "", true, FattyAcids);
+    public static readonly Nutrient CisOctadecadienoic =
+        new(nameof(CisOctadecadienoic), (int)NutrientToken.CisOctadecadienoic, "Ácido cis-octadecadienoico", "", false,
+            Polyunsaturated);
 
-    public static readonly Nutrient TetradecenoicAcid =
-        new(nameof(TetradecenoicAcid), (int)NutrientToken.TetradecenoicAcid, "Ácido tetradecenoico",
-            "Ácido miristoleico", false, MonounsaturatedFattyAcids);
+    public static readonly Nutrient Ioctadecadienoic =
+        new(nameof(Ioctadecadienoic), (int)NutrientToken.Ioctadecadienoic, "Ácido i-octadecadienoico", "", false,
+            Polyunsaturated);
 
-    public static readonly Nutrient PentadecenoicAcid =
-        new(nameof(PentadecenoicAcid), (int)NutrientToken.PentadecenoicAcid, "Ácido pentadecenoico", "", false,
-            MonounsaturatedFattyAcids);
+    public static readonly Nutrient ConOctadecadienoic =
+        new(nameof(ConOctadecadienoic), (int)NutrientToken.ConOctadecadienoic, "Ácido octadecadienoico conjugado", "",
+            false, Polyunsaturated);
 
-    public static readonly Nutrient HexadecenoicAcid =
-        new(nameof(HexadecenoicAcid), (int)NutrientToken.HexadecenoicAcid, "Ácido hexadecenoico", "Ácido palmitoleico",
-            false, MonounsaturatedFattyAcids);
+    public static readonly Nutrient Octadecatrienoic =
+        new(nameof(Octadecatrienoic), (int)NutrientToken.Octadecatrienoic, "Ácido octadecatrienoico",
+            "Ácido linolénico", false, Polyunsaturated);
 
-    public static readonly Nutrient CisHexadecenoicAcid =
-        new(nameof(CisHexadecenoicAcid), (int)NutrientToken.CisHexadecenoicAcid, "Ácido cis-hexadecenoico", "", false,
-            MonounsaturatedFattyAcids);
+    public static readonly Nutrient Cis3Octadecatrienoic =
+        new(nameof(Cis3Octadecatrienoic), (int)NutrientToken.Cis3Octadecatrienoic, "Ácido cis3-octadecatrienoico",
+            "Ácido α-linolénico", false, Polyunsaturated);
 
-    public static readonly Nutrient HeptadecenoicAcid =
-        new(nameof(HeptadecenoicAcid), (int)NutrientToken.HeptadecenoicAcid, "Ácido heptadecenoico", "", false,
-            MonounsaturatedFattyAcids);
+    public static readonly Nutrient Cis6Octadecatrienoic =
+        new(nameof(Cis6Octadecatrienoic), (int)NutrientToken.Cis6Octadecatrienoic, "Ácido cis6-octadecatrienoico",
+            "Ácido γ-linolénico", false, Polyunsaturated);
 
-    public static readonly Nutrient IheptadecenoicAcid =
-        new(nameof(IheptadecenoicAcid), (int)NutrientToken.IheptadecenoicAcid, "Ácido i-heptadecenoico", "", false,
-            MonounsaturatedFattyAcids);
+    public static readonly Nutrient Ioctadecatrienoic =
+        new(nameof(Ioctadecatrienoic), (int)NutrientToken.Ioctadecatrienoic, "Ácido i-octadecatrienoico", "", false,
+            Polyunsaturated);
 
-    public static readonly Nutrient OctadecenoicAcid =
-        new(nameof(OctadecenoicAcid), (int)NutrientToken.OctadecenoicAcid, "Ácido octadecenoico", "Ácido oleico", false,
-            MonounsaturatedFattyAcids);
+    public static readonly Nutrient Octadecatetraenoic =
+        new(nameof(Octadecatetraenoic), (int)NutrientToken.Octadecatetraenoic, "Ácido octadecatetraenoico", "", false,
+            Polyunsaturated);
 
-    public static readonly Nutrient CisOctadecenoicAcid =
-        new(nameof(CisOctadecenoicAcid), (int)NutrientToken.CisOctadecenoicAcid, "Ácido cis-octadecenoico", "", false,
-            MonounsaturatedFattyAcids);
+    public static readonly Nutrient Eicosadienoic =
+        new(nameof(Eicosadienoic), (int)NutrientToken.Eicosadienoic, "Ácido eicosadienoico", "", false,
+            Polyunsaturated);
 
-    public static readonly Nutrient EicosenoicAcid =
-        new(nameof(EicosenoicAcid), (int)NutrientToken.EicosenoicAcid, "Ácido eicosenoico", "Ácido gadoleico", false,
-            MonounsaturatedFattyAcids);
+    public static readonly Nutrient Eicosatrienoic =
+        new(nameof(Eicosatrienoic), (int)NutrientToken.Eicosatrienoic, "Ácido eicosatrienoico", "", false,
+            Polyunsaturated);
 
-    public static readonly Nutrient DocosenoicAcid =
-        new(nameof(DocosenoicAcid), (int)NutrientToken.DocosenoicAcid, "Ácido docosenoico", "", false,
-            MonounsaturatedFattyAcids);
+    public static readonly Nutrient N3Eicosatrienoic =
+        new(nameof(N3Eicosatrienoic), (int)NutrientToken.N3Eicosatrienoic, "Ácido 3-eicosatrienoico", "", false,
+            Polyunsaturated);
 
-    public static readonly Nutrient CisDocosenoicAcid =
-        new(nameof(CisDocosenoicAcid), (int)NutrientToken.CisDocosenoicAcid, "Ácido cis-docosenoico", "Ácido erúcico",
-            false, MonounsaturatedFattyAcids);
+    public static readonly Nutrient N6Eicosatrienoic =
+        new(nameof(N6Eicosatrienoic), (int)NutrientToken.N6Eicosatrienoic, "Ácido 6-eicosatrienoico", "", false,
+            Polyunsaturated);
 
-    public static readonly Nutrient CisTetracosenoicAcid =
-        new(nameof(CisTetracosenoicAcid), (int)NutrientToken.CisTetracosenoicAcid, "Ácido cis-tetracosenoico",
-            "Ácido nervónico", false, MonounsaturatedFattyAcids);
+    public static readonly Nutrient Eicosatetraenoic =
+        new(nameof(Eicosatetraenoic), (int)NutrientToken.Eicosatetraenoic, "Ácido eicosatetraenoico", "", false,
+            Polyunsaturated);
+
+    public static readonly Nutrient N6Eicosatetraenoic =
+        new(nameof(N6Eicosatetraenoic), (int)NutrientToken.N6Eicosatetraenoic, "Ácido 6-eicosatetraenoico",
+            "Ácido araquidónico", false, Polyunsaturated);
+
+    public static readonly Nutrient Eicosapentaenoic =
+        new(nameof(Eicosapentaenoic), (int)NutrientToken.Eicosapentaenoic, "Ácido eicosapentaenoico",
+            "Ácido timnodónico", false, Polyunsaturated);
+
+    public static readonly Nutrient N3Docosapentaenoic =
+        new(nameof(N3Docosapentaenoic), (int)NutrientToken.N3Docosapentaenoic, "Ácido 3-docosapentaenoico", "", false,
+            Polyunsaturated);
+
+    public static readonly Nutrient N3Docosahexaenoic =
+        new(nameof(N3Docosahexaenoic), (int)NutrientToken.N3Docosahexaenoic, "Ácido 3-docosahexaenoico", "", false,
+            Polyunsaturated);
+    
+    public static readonly Nutrient Trans =
+        new(nameof(Trans), (int)NutrientToken.Trans, "Ácidos grasos trans, total", "", false, null);
+
+    public static readonly Nutrient TransMonoenoic =
+        new(nameof(TransMonoenoic), (int)NutrientToken.TransMonoenoic, "Ácido 3-docosapentaenoico", "", false, Trans);
+
+    public static readonly Nutrient TransPolinoic =
+        new(nameof(TransPolinoic), (int)NutrientToken.TransPolinoic, "Ácido 3-docosahexaenoico", "", false, Trans);
 
     public static readonly Nutrient Minerals =
         new(nameof(Minerals), (int)NutrientToken.Minerals, "Minerales", "", false, null);
@@ -211,7 +286,9 @@ public class Nutrient : SmartEnum<Nutrient>, IHierarchicalEnum<Nutrient, Nutrien
 public enum NutrientToken
 {
     None,
+    // Energy
     Energy,
+    // Carbohydrates
     Carbohydrates,
     Fiber,
     Sugars,
@@ -222,22 +299,63 @@ public enum NutrientToken
     Maltose,
     Galactose,
     Starch,
+    // Fatty Acids
     FattyAcids,
-    SaturatedFattyAcids,
-    ButanoicAcid,
-    HexanoicAcid,
-    OctanoicAcid,
-    DecanoicAcid,
-    DodecanoicAcid,
-    TridecanoicAcid,
-    TetradecanoicAcid,
-    PentadecanoicAcid,
-    HexadecanoicAcid,
-    HeptadecanoicAcid,
-    OctadecanoicAcid,
-    EicosanoicAcid,
-    DocosanoicAcid,
-    TetracosanoicAcid,
+    // Saturated Fatty Acids
+    Saturated,
+    Butanoic,
+    Hexanoic,
+    Octanoic,
+    Decanoic,
+    Dodecanoic,
+    Tridecanoic,
+    Tetradecanoic,
+    Pentadecanoic,
+    Hexadecanoic,
+    Heptadecanoic,
+    Octadecanoic,
+    Eicosanoic,
+    Docosanoic,
+    Tetracosanoic,
+    // Monounsaturated Fatty Acids
+    Monounsaturated,
+    Tetradecenoic,
+    Pentadecenoic,
+    Hexadecenoic,
+    CisHexadecenoic,
+    Heptadecenoic,
+    Iheptadecenoic,
+    Octadecenoic,
+    CisOctadecenoic,
+    Eicosenoic,
+    Docosenoic,
+    CisDocosenoic,
+    CisTetracosenoic,
+    // Polyunsaturated Fatty Acids
+    Polyunsaturated,
+    Octadecadienoic,
+    CisOctadecadienoic,
+    Ioctadecadienoic,
+    ConOctadecadienoic,
+    Octadecatrienoic,
+    Cis3Octadecatrienoic,
+    Cis6Octadecatrienoic,
+    Ioctadecatrienoic,
+    Octadecatetraenoic,
+    Eicosadienoic,
+    Eicosatrienoic,
+    N3Eicosatrienoic,
+    N6Eicosatrienoic,
+    Eicosatetraenoic,
+    N6Eicosatetraenoic,
+    Eicosapentaenoic,
+    N3Docosapentaenoic,
+    N3Docosahexaenoic,
+    // Trans Fatty Acids
+    Trans,
+    TransMonoenoic,
+    TransPolinoic,
+    // Minerals
     Minerals,
     Calcium,
     Iron,
@@ -250,17 +368,4 @@ public enum NutrientToken
     Manganese,
     Selenium,
     Fluoride,
-    MonounsaturatedFattyAcids,
-    TetradecenoicAcid,
-    PentadecenoicAcid,
-    HexadecenoicAcid,
-    CisHexadecenoicAcid,
-    HeptadecenoicAcid,
-    IheptadecenoicAcid,
-    OctadecenoicAcid,
-    CisOctadecenoicAcid,
-    EicosenoicAcid,
-    DocosenoicAcid,
-    CisDocosenoicAcid,
-    CisTetracosenoicAcid
 }
