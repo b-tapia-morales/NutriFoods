ebeb2e253afd7e3ce50b9fc29f487f4e6463dc0e
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-11-11 12:53:22 -0300

    Generified and simplified the way of making request for ingredients and retrieving Nutritional Info

--- NutrientRetrieval/FullRetrieval/FullRetrieval.cs
+++ NutrientRetrieval/FullRetrieval/FullRetrieval.cs
@@ -1,58 +1,14 @@
 using Domain.Models;
 using NutrientRetrieval.Food;
 using NutrientRetrieval.Translation;
-using Utils.Enum;
 
 namespace NutrientRetrieval.FullRetrieval;
 
-public class FullRetrieval : IFoodRetrieval<Food>
+public class FullRetrieval : IFoodRetrieval<Food, FoodNutrient>
 {
-    private const string ConnectionString =
-        "Host=localhost;Database=nutrifoods_db;Username=nutrifoods_dev;Password=MVmYneLqe91$";
+    public string Format => "full";
 
-    private const string Format = "full";
-
-    private static readonly IFoodRetrieval<Food> Instance = new FullRetrieval();
-
-    public static void RetrieveFromApi() => Instance.RetrieveFromApi(ConnectionString, Format);
-
-    public void InsertNutrients(NutrifoodsDbContext context, IReadOnlyDictionary<string, int> dictionary,
-        int ingredientId, Food food) => s_InsertNutrients(context, dictionary, ingredientId, food);
-
-    public void InsertMeasures(NutrifoodsDbContext context, int ingredientId, Food food) =>
-        s_InsertMeasures(context, ingredientId, food);
-
-    private static void s_InsertNutrients(NutrifoodsDbContext context, IReadOnlyDictionary<string, int> dictionary,
-        int ingredientId, Food food)
-    {
-        if (food.FoodNutrients.Length == 0) return;
-
-        foreach (var foodNutrient in food.FoodNutrients)
-        {
-            var fdcNutrientId = foodNutrient.Nutrient.Number;
-            if (!dictionary.ContainsKey(fdcNutrientId)) continue;
-
-            var code = foodNutrient.Nutrient.UnitName switch
-            {
-                "g" or "G" => 1,
-                "mg" or "MG" => 2,
-                "µg" or "UG" => 3,
-                "kcal" or "KCAL" => 4,
-                _ => throw new ArgumentException("Code is not recognized")
-            };
-
-            var nutrientId = dictionary[fdcNutrientId];
-            context.IngredientNutrients.Add(new IngredientNutrient
-            {
-                IngredientId = ingredientId,
-                NutrientId = nutrientId,
-                Quantity = foodNutrient.Amount,
-                Unit = UnitEnum.FromValue(code)
-            });
-        }
-    }
-
-    private static void s_InsertMeasures(NutrifoodsDbContext context, int ingredientId, Food food)
+    public void InsertMeasures(NutrifoodsDbContext context, int ingredientId, Food food)
     {
         if (food.FoodPortions.Length == 0) return;
 
--- NutrientRetrieval/Request/DataCentral.cs
+++ NutrientRetrieval/Request/DataCentral.cs
@@ -4,6 +4,9 @@ using NutrientRetrieval.Food;
 using Utils.Csv;
 using Utils.Enumerable;
 using static Utils.Csv.DelimiterToken;
+using JsonException = Newtonsoft.Json.JsonException;
+
+// ReSharper disable MemberCanBePrivate.Global
 
 namespace NutrientRetrieval.Request;
 
@@ -22,39 +25,72 @@ public static class DataCentral
 
     private static readonly HttpClient Client = new();
 
-    public static async Task<(int Id, T Food)> FetchItem<T>(int nutriFoodsId, int foodDataCentralId, string format)
-        where T : IFood
+    public static async Task<Dictionary<int, TFood>> PerformRequest<TFood, TNutrient>(string format,
+        RequestMethod method = RequestMethod.Multiple)
+        where TFood : class, IFood<TNutrient>
+        where TNutrient : class, IFoodNutrient =>
+        method switch
+        {
+            RequestMethod.Single => await RetrieveByItem<TFood, TNutrient>(format),
+            RequestMethod.Multiple => await RetrieveByList<TFood, TNutrient>(format),
+            _ => throw new ArgumentOutOfRangeException(nameof(method), method, null)
+        };
+
+    public static async Task<Dictionary<int, TFood>> RetrieveByItem<TFood, TNutrient>(string format)
+        where TFood : class, IFood<TNutrient>
+        where TNutrient : class, IFoodNutrient
+    {
+        var dictionary = RowRetrieval.RetrieveRows<IngredientRow, IngredientMapping>(FilePath, Semicolon, true)
+            .Where(e => e.FoodDataCentralId != null)
+            .ToDictionary(e => e.NutriFoodsId, e => e.FoodDataCentralId.GetValueOrDefault());
+        var tasks = dictionary.Select(e => FetchItem<TFood, TNutrient>(e.Key, e.Value, format));
+        var tuples = await Task.WhenAll(tasks);
+        return tuples.ToDictionary(tuple => tuple.Id, tuple => tuple.Food);
+    }
+
+    public static async Task<Dictionary<int, TFood>> RetrieveByList<TFood, TNutrient>(string format)
+        where TFood : class, IFood<TNutrient>
+        where TNutrient : class, IFoodNutrient
+    {
+        // Takes all the CSV rows, filters those which have no corresponding FoodDataCentral Id, removes duplicate Ids,
+        // and then it converts them to a dictionary which contains the FoodDataCentral and NutriFoods ids as keys
+        // and values respectively.
+        var dictionary = RowRetrieval.RetrieveRows<IngredientRow, IngredientMapping>(FilePath, Semicolon, true)
+            .Where(e => e.FoodDataCentralId != null)
+            .DistinctBy(e => e.FoodDataCentralId)
+            .ToDictionary(e => e.FoodDataCentralId.GetValueOrDefault(), e => e.NutriFoodsId);
+        // Partitions the dictionary into a list of Key-Pairs of 20 items each.
+        var pairs = EnumerableUtils.Partition(dictionary, MaxItemsPerRequest);
+        // Converts each list of Key-Pairs into a Task which performs the retrieval from FoodDataCentral.
+        // This is done so each request can be performed concurrently.
+        var tasks = pairs.Select(e => FetchList<TFood, TNutrient>(new Dictionary<int, int>(e), format));
+        // Awaits until each task is completed.
+        var tuplesList = await Task.WhenAll(tasks);
+        // The resulting value from the request is a list of lists of named tuples, which need to be flattened into
+        // a single list of named tuples. The last step is transforming each named tuple into a dictionary, which
+        // takes each NutriFoods Id and food item as the keys and values respectively.
+        return tuplesList.SelectMany(e => e).ToDictionary(e => e.Id, e => e.Food);
+    }
+
+    private static async Task<(int Id, TFood Food)> FetchItem<TFood, TNutrient>(int nutriFoodsId, int foodDataCentralId,
+        string format)
+        where TFood : class, IFood<TNutrient>
+        where TNutrient : class, IFoodNutrient
     {
         var path = $"https://api.nal.usda.gov/fdc/v1/food/{foodDataCentralId}?format={format}&api_key={ApiKey}";
         var uri = new Uri(path);
 
         var response = await Client.GetAsync(uri);
         var serialized = await response.Content.ReadAsStringAsync();
-        var food = JsonConvert.DeserializeObject<T>(serialized) ?? throw new NullReferenceException();
+        var food = JsonConvert.DeserializeObject<TFood>(serialized) ?? throw new JsonException();
 
         return (nutriFoodsId, food);
     }
 
-    /// <summary>
-    ///     Fetches a list of 20 food items at most from FoodDataCentral's Api, and returns a list of named tuples whose
-    ///     <c>Id</c> and <c>Food</c> data fields correspond to the NutriFoods Id and the FoodDataCentral food item
-    ///     respectively.
-    /// </summary>
-    /// <param name="dictionary">
-    ///     The dictionary which contains the FoodDataCentral and NutriFoods ids as keys and values
-    ///     respectively.
-    /// </param>
-    /// <param name="format">The format used, which can be either <i>full</i> or <i>abridged</i>.</param>
-    /// <typeparam name="T">The provided class must implement <c>IFood</c>.</typeparam>
-    /// <returns>A list of 20 named tuples at most.</returns>
-    /// <exception cref="NullReferenceException">
-    ///     The request failed due to not following the request format specified by
-    ///     FoodDataCentral. See
-    ///     <a href="https://app.swaggerhub.com/apis/fdcnal/food-data_central_api/1.0.1#/FDC/getFoods">here</a>
-    ///     for reference.
-    /// </exception>
-    public static async Task<IEnumerable<(int Id, T Food)>> FetchList<T>(
-        Dictionary<int, int> dictionary, string format) where T : IFood
+    private static async Task<IEnumerable<(int Id, TFood Food)>> FetchList<TFood, TNutrient>(
+        IReadOnlyDictionary<int, int> dictionary, string format)
+        where TFood : class, IFood<TNutrient>
+        where TNutrient : class, IFoodNutrient
     {
         // Takes the dictionary's keys, appends 'fdcIds' to each one of them, and concatenates them using the '&' delimiter.
         // FoodDataCentral's Api can take either comma separated parameters, or repeating parameters (the second one is the approach used here).
@@ -64,66 +100,10 @@ public static class DataCentral
 
         var response = await Client.GetAsync(uri);
         var serialized = await response.Content.ReadAsStringAsync();
-        var list = JsonConvert.DeserializeObject<List<T>>(serialized) ?? throw new NullReferenceException();
+        var list = JsonConvert.DeserializeObject<List<TFood>>(serialized) ?? throw new JsonException();
 
         // The resulting list can be converted into a named tuple using the food item's Id as a key to retrieve from the dictionary its corresponding NutriFoods' Id .
-        return list.Select(e => (dictionary[e.FdcId()], e));
-    }
-
-    public static async Task<Dictionary<int, T>> RetrieveByItem<T>(string format) where T : IFood
-    {
-        var dictionary = RowRetrieval.RetrieveRows<IngredientRow, IngredientMapping>(FilePath, Semicolon, true)
-            .Where(e => e.FoodDataCentralId != null)
-            .ToDictionary(e => e.NutriFoodsId, e => e.FoodDataCentralId.GetValueOrDefault());
-        var tasks = dictionary.Select(e => FetchItem<T>(e.Key, e.Value, format));
-        var tuples = await Task.WhenAll(tasks);
-        return tuples.ToDictionary(tuple => tuple.Id, tuple => tuple.Food);
-    }
-
-    /// <summary>
-    /// Fetches all FoodDataCentral and NutriFoods ids specified in the corresponding CSV file, performs a request to
-    /// FoodDataCentral's Api in batches of 20 food items each, and returns a dictionary which contains the NutriFoods
-    /// Ids and food items themselves as the keys and values respectively.
-    /// </summary>
-    /// <param name="format">The format used, which can be either <i>full</i> or <i>abridged</i>.</param>
-    /// <typeparam name="T">The provided class must implement <c>IFood</c>.</typeparam>
-    /// <returns>A dictionary which contains the NutriFoods Ids and food items themselves as the keys and values
-    /// respectively.</returns>
-    public static async Task<Dictionary<int, T>> RetrieveByList<T>(string format) where T : IFood
-    {
-        // Takes all the CSV rows, filters those which have no corresponding FoodDataCentral Id, removes duplicate Ids,
-        // and then it converts them to a dictionary which contains the FoodDataCentral and NutriFoods ids as keys
-        // and values respectively.
-        var dictionary = RowRetrieval.RetrieveRows<IngredientRow, IngredientMapping>(FilePath, Semicolon, true)
-            .Where(e => e.FoodDataCentralId != null)
-            .DistinctBy(e => e.FoodDataCentralId)
-            .ToDictionary(e => e.FoodDataCentralId.GetValueOrDefault(), e => e.NutriFoodsId);
-        // Partitions the dictionary into chunks of 20 items each.
-        var dictionaryList = EnumerableUtils.Partition(dictionary, MaxItemsPerRequest);
-        // Converts each dictionary partition into a Task which performs the retrieval from FoodDataCentral.
-        // This is done so each request can be performed concurrently.
-        var tasks = dictionaryList.Select(e => FetchList<T>(new Dictionary<int, int>(e), format));
-        // Awaits until each task is completed.
-        var tuplesList = await Task.WhenAll(tasks);
-        // The resulting value from the request is a list of lists of named tuples, which need to be flattened into
-        // a single list of named tuples. The last step is transforming each named tuple into a dictionary, which
-        // takes each NutriFoods Id and food item as the keys and values respectively.
-        return tuplesList.SelectMany(e => e).ToDictionary(e => e.Id, e => e.Food);
-    }
-
-    public static async Task<Dictionary<int, T>> PerformRequest<T>(RequestMethod method, string format) where T : IFood
-    {
-        return method switch
-        {
-            RequestMethod.Single => await RetrieveByItem<T>(format),
-            RequestMethod.Multiple => await RetrieveByList<T>(format),
-            _ => throw new ArgumentOutOfRangeException(nameof(method), method, null)
-        };
-    }
-
-    public static async Task<Dictionary<int, T>> PerformRequest<T>(string format) where T : IFood
-    {
-        return await RetrieveByList<T>(format);
+        return list.Select(e => (dictionary[e.FdcId], e));
     }
 }
 
