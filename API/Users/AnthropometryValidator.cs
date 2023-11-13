8d4d83d1a39dae8f5a8a2518ca07db1f656cc63f
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-11-13 13:01:51 -0300

    Removed unused classes, reworked remaining validators that might still be of use

--- API/Users/AnthropometryValidator.cs
+++ API/Users/AnthropometryValidator.cs
@@ -1,13 +1,12 @@
 using API.Dto;
 using FluentValidation;
 using Newtonsoft.Json;
-using Utils.Enum;
 
 namespace API.Users;
 
-public class UserBodyMetricValidator : AbstractValidator<UserBodyMetricDto>
+public class AnthropometryValidator : AbstractValidator<AnthropometryDto>
 {
-    public UserBodyMetricValidator()
+    public AnthropometryValidator()
     {
         RuleFor(e => e.Height).InclusiveBetween(150, 200)
             .WithMessage(e =>
@@ -17,16 +16,9 @@ public class UserBodyMetricValidator : AbstractValidator<UserBodyMetricDto>
             .WithMessage(e =>
                 JsonConvert.ToString(
                     $"User's weight is not within the allowed range (User's weight: {Math.Round(e.Weight, 2)} - Allowed weight range: 50 - 200 [kg])."));
-        RuleFor(e => e.BodyMassIndex).InclusiveBetween(16.00, 34.99)
+        RuleFor(e => e.Bmi).InclusiveBetween(16.00, 34.99)
             .WithMessage(e =>
                 JsonConvert.ToString(
-                    $"User's BMI is not within the allowed range (User's BMI: {Math.Round(e.BodyMassIndex, 2)} - Allowed BMI range: 16.00 - 34.99)."));
-        RuleFor(e => e.PhysicalActivity)
-            .Must(e => PhysicalActivityEnum.FromReadableName(e) != null)
-            .WithMessage(e =>
-                JsonConvert.ToString($@"
-Provided argument “{e}” does not correspond to a valid physical activity value.
-Recognized values are:
-{string.Join('\n', PhysicalActivityEnum.List)}"));
+                    $"User's BMI is not within the allowed range (User's BMI: {Math.Round(e.Bmi, 2)} - Allowed BMI range: 16.00 - 34.99)."));
     }
 }
--- API/Users/UserValidator.cs
+++ API/Users/UserValidator.cs
@@ -5,7 +5,7 @@ using Utils;
 
 namespace API.Users;
 
-public class UserValidator : AbstractValidator<UserDto>
+public class UserValidator : AbstractValidator<NutritionistDto>
 {
     public UserValidator()
     {
@@ -16,10 +16,13 @@ public class UserValidator : AbstractValidator<UserDto>
                 JsonConvert.ToString($"Provided argument “{e.Email}” does not correspond to a valid email.")));
 
         // Username
-        When(e => !string.IsNullOrWhiteSpace(e.Username), () => RuleFor(e => e.Username).Matches(RegexUtils.Username)
-            .WithMessage(e =>
-                JsonConvert.ToString($@"
-Provided argument “{e.Username}” does not match required validation rules for username.
-{RegexUtils.UsernameRule}")));
+        When(e => !string.IsNullOrWhiteSpace(e.Username), () => RuleFor(e => e.Username)
+            .Matches(RegexUtils.Username)
+            .WithMessage(e => JsonConvert.ToString(
+                $"""
+                 Provided argument “{e.Username}” does not match required validation rules for username.
+                 {RegexUtils.UsernameRule}
+                 """))
+        );
     }
 }
