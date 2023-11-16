c38d8e4c05d27cb260ab4be32f55c0867c2e5a30
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-11-16 00:52:56 -0300

    Fixed bug with capitalization

--- Utils/String/StringExtensions.cs
+++ Utils/String/StringExtensions.cs
@@ -8,13 +8,17 @@ public static class StringExtensions
 {
     public static string Capitalize(this string str)
     {
+        if (string.IsNullOrWhiteSpace(str))
+            return string.Empty;
         var span = str.AsSpan().Trim();
-        return $"{char.ToUpper(span[0])}{span[1..]}";
+        return $"{char.ToUpper(span[0])}{span[1..].ToString()}";
     }
 
     public static string RemoveAccents(this string str) =>
-        string.Concat(
-            str.Normalize(FormD)
-                .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != NonSpacingMark)
-        ).Normalize(FormC);
+        string.IsNullOrWhiteSpace(str)
+            ? string.Empty
+            : string.Concat(
+                str.Normalize(FormD)
+                    .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != NonSpacingMark)
+            ).Normalize(FormC);
 }
