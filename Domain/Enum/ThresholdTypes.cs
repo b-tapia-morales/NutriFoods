c55313e4a33e33048ff147904db6e6de0f911a44
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-11-22 01:03:29 -0300

    Minor QOL for variable name now clearly indicating whether it'a priority or not

--- Domain/Enum/ThresholdTypes.cs
+++ Domain/Enum/ThresholdTypes.cs
@@ -9,9 +9,9 @@ public class ThresholdTypes : SmartEnum<ThresholdTypes>, IEnum<ThresholdTypes, T
 
     public static readonly ThresholdTypes WithinRange =
         new(nameof(WithinRange), (int)ThresholdToken.WithinRange, "Lo más exacto posible",
-            (targetValue, actualValue, errorMargin, isMacronutrient) =>
+            (targetValue, actualValue, errorMargin, isPriority) =>
             {
-                var divisor = isMacronutrient ? +1 : +2;
+                var divisor = isPriority ? +1 : +2;
                 if (targetValue * (1 - errorMargin / 2) <= actualValue &&
                     targetValue * (1 + errorMargin / 2) >= actualValue)
                     return +2 / divisor;
@@ -24,17 +24,17 @@ public class ThresholdTypes : SmartEnum<ThresholdTypes>, IEnum<ThresholdTypes, T
 
     public static readonly ThresholdTypes AtLeast =
         new(nameof(AtLeast), (int)ThresholdToken.AtLeast, "A lo menos",
-            (targetValue, actualValue, errorMargin, isMacronutrient) =>
+            (targetValue, actualValue, errorMargin, isPriority) =>
             {
-                var divisor = isMacronutrient ? +1 : +2;
+                var divisor = isPriority ? +1 : +2;
                 return (1 + errorMargin) * targetValue >= actualValue ? +2 / divisor : -2 / divisor;
             });
 
     public static readonly ThresholdTypes AtMost =
         new(nameof(AtMost), (int)ThresholdToken.AtMost, "A lo más",
-            (targetValue, actualValue, errorMargin, isMacronutrient) =>
+            (targetValue, actualValue, errorMargin, isPriority) =>
             {
-                var divisor = isMacronutrient ? +1 : +2;
+                var divisor = isPriority ? +1 : +2;
                 return (1 + errorMargin) * targetValue <= actualValue ? +2 / divisor : -2 / divisor;
             });
 
