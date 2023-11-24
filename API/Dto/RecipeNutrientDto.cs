a50679b0de3d4fa0a08dbff13189a58e8197b0b4
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-11-24 00:39:30 -0300

    Removed now unused Nutrient tables for ingredient and recipe DTOs

--- API/Dto/RecipeNutrientDto.cs
+++
@@ -1,8 +0,0 @@
-namespace API.Dto;
-
-public class RecipeNutrientDto
-{
-    public string Nutrient { get; set; } = null!;
-    public double Quantity { get; set; }
-    public string Unit { get; set; } = null!;
-}
