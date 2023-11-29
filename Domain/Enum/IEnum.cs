c0d2c873176b9e4715279b3a65ef272eb4ce3385
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-11-29 01:33:59 -0300

    Readable Name dict now is able to preserve the token sorting order

--- Domain/Enum/IEnum.cs
+++ Domain/Enum/IEnum.cs
@@ -1,6 +1,7 @@
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using Ardalis.SmartEnum;
+using Utils.Enumerable;
 
 namespace Domain.Enum;
 
@@ -9,19 +10,20 @@ public interface IEnum<out T, TEnum>
     where T : SmartEnum<T>, IEnum<T, TEnum>
     where TEnum : struct, System.Enum, IConvertible
 {
+    private static readonly IComparer<string> Comparer = new ReadableNameComparer<T, TEnum>();
+
     private static readonly ImmutableList<TEnum> Tokens =
         System.Enum.GetValues<TEnum>().OrderBy(e => e.ToInt32(null)).ToImmutableList();
 
-    static ImmutableList<T> Values { get; } = SmartEnum<T>.List.OrderBy(e => e.Value).ToImmutableList();
+    static IReadOnlyList<T> Values { get; } = SmartEnum<T>.List.OrderBy(e => e.Value).ToImmutableList();
 
-    static ImmutableList<T> NonNullValues { get; } = Values.Skip(1).ToImmutableList();
+    static IReadOnlyList<T> NonNullValues { get; } = Values.Skip(1).ToImmutableList();
 
-    static ImmutableSortedDictionary<TEnum, T> TokenDictionary { get; } =
+    static IReadOnlyDictionary<TEnum, T> TokenDictionary { get; } =
         Tokens.Zip(Values, (k, v) => new { Key = k, Value = v }).ToImmutableSortedDictionary(e => e.Key, e => e.Value);
 
-    static ImmutableDictionary<string, T> ReadableNameDictionary { get; } =
-        Tokens.Zip(Values, (k, v) => new { Key = k, Value = v })
-            .ToImmutableDictionary(e => e.Value.ReadableName, e => e.Value);
+    static IReadOnlyDictionary<string, T> ReadableNameDictionary { get; } =
+        Values.ToSortedDictionary(e => e.ReadableName, e => e, Comparer).AsReadOnly();
 
     static IReadOnlyDictionary<T, TEnum> ReverseTokenDictionary { get; } =
         Tokens.Zip(Values, (k, v) => new { Key = k, Value = v }).ToImmutableSortedDictionary(e => e.Value, e => e.Key);
@@ -38,7 +40,9 @@ public interface IEnum<out T, TEnum>
         ReverseTokenDictionary.TryGetValue(value, out var token) ? token : throw new KeyNotFoundException();
 
     static TEnum ToToken(string readableName) =>
-        ReadableNameDictionary.TryGetValue(readableName, out var value) ? ToToken(value) : throw new KeyNotFoundException();
+        ReadableNameDictionary.TryGetValue(readableName, out var value)
+            ? ToToken(value)
+            : throw new KeyNotFoundException();
 
     static string ToReadableName(TEnum token) => ToValue(token).ReadableName;
 }
@@ -57,4 +61,23 @@ public interface IHierarchicalEnum<out T, TEnum> : IEnum<T, TEnum>, IComposableE
     where T : SmartEnum<T>, IHierarchicalEnum<T, TEnum>
     where TEnum : struct, System.Enum, IConvertible
 {
+}
+
+public class ReadableNameComparer<T, TEnum> : IComparer<string>
+    where T : SmartEnum<T>, IEnum<T, TEnum>
+    where TEnum : struct, System.Enum, IConvertible
+{
+    public int Compare(string? x, string? y)
+    {
+        var dict = SmartEnum<T>.List.ToImmutableSortedDictionary(e => e.ReadableName, e => e.Value);
+        if (ReferenceEquals(x, y))
+            return +0;
+        if (ReferenceEquals(x, null))
+            return -1;
+        if (ReferenceEquals(y, null))
+            return +1;
+        if (!dict.TryGetValue(x, out var first) || !dict.TryGetValue(y, out var second))
+            throw new KeyNotFoundException();
+        return first.CompareTo(second);
+    }
 }
