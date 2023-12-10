1d4bdbea6eb2914a1bc075933895fec508198ae7
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-12-10 13:04:27 -0300

    Fixed bug with datetime format

--- API/Dto/ClinicalAnamnesisDto.cs
+++ API/Dto/ClinicalAnamnesisDto.cs
@@ -2,8 +2,8 @@
 
 public sealed class ClinicalAnamnesisDto
 {
-    public string? CreatedOn { get; set; }
-    public string? LastUpdated { get; set; }
+    public DateTime? CreatedOn { get; set; }
+    public DateTime? LastUpdated { get; set; }
     public ICollection<ClinicalSignDto> ClinicalSigns { get; set; } = null!;
     public ICollection<DiseaseDto> Diseases { get; set; } = null!;
     public ICollection<IngestibleDto> Ingestibles { get; set; } = null!;
