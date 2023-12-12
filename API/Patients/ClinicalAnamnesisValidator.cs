362d1470e91e4fac5eaa8d445bc48f7dcb98e980
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-12-12 02:04:16 -0300

    Also added validations for administration times

--- API/Patients/ClinicalAnamnesisValidator.cs
+++ API/Patients/ClinicalAnamnesisValidator.cs
@@ -2,6 +2,7 @@ using API.Dto;
 using API.Validations;
 using Domain.Enum;
 using FluentValidation;
+using Utils;
 
 namespace API.Patients;
 
@@ -23,39 +24,47 @@ public class ClinicalAnamnesisValidator : AbstractValidator<ClinicalAnamnesisDto
                     .WithMessage(e => MessageExtensions.StringLength("name", e.Name));
             });
         RuleForEach(e => e.Diseases)
-            .ChildRules(outerChild =>
+            .ChildRules(outer =>
             {
-                outerChild.RuleFor(e => e.Name)
+                outer.RuleFor(e => e.Name)
                     .Must(e => !string.IsNullOrWhiteSpace(e) && e.Length > 2)
                     .WithMessage(e => MessageExtensions.StringLength("name", e.Name));
-                outerChild.RuleForEach(e => e.InheritanceTypes)
-                    .ChildRules(innerChild =>
+                outer.RuleForEach(e => e.InheritanceTypes)
+                    .ChildRules(inner =>
                     {
-                        innerChild
+                        inner
                             .RuleFor(e => e)
                             .Must(e => IEnum<InheritanceTypes, InheritanceToken>.ReadableNameDictionary.ContainsKey(e))
                             .WithMessage(MessageExtensions.NotInEnum<InheritanceTypes, InheritanceToken>);
                     });
             });
         RuleForEach(e => e.Ingestibles)
-            .ChildRules(c =>
+            .ChildRules(outer =>
             {
-                c.RuleFor(e => e.Name)
+                outer.RuleFor(e => e.Name)
                     .Must(e => !string.IsNullOrWhiteSpace(e) && e.Length > 2)
                     .WithMessage(e => MessageExtensions.StringLength("name", e.Name));
-                c.RuleFor(e => e.Type)
+                outer.RuleFor(e => e.Type)
                     .Must(e => IEnum<IngestibleTypes, IngestibleToken>.ReadableNameDictionary.ContainsKey(e))
                     .WithMessage(e => MessageExtensions.NotInEnum<IngestibleTypes, IngestibleToken>(e.Type));
-                c.RuleFor(e => e.Adherence)
+                outer.RuleFor(e => e.Adherence)
                     .Must(e => IEnum<Frequencies, FrequencyToken>.ReadableNameDictionary.ContainsKey(e))
                     .WithMessage(e => MessageExtensions.NotInEnum<Frequencies, FrequencyToken>(e.Adherence));
-                c.RuleFor(e => e.Dosage)
+                outer.RuleFor(e => e.Dosage)
                     .GreaterThan(0)
                     .When(e => e.Dosage != null);
-                c.RuleFor(e => e.Unit)
+                outer.RuleFor(e => e.Unit)
                     .Must(e => IEnum<Units, UnitToken>.ReadableNameDictionary.ContainsKey(e!))
                     .When(e => e != null)
                     .WithMessage(e => MessageExtensions.NotInEnum<Units, UnitToken>(e.Unit!));
+                outer.RuleForEach(e => e.AdministrationTimes)
+                    .ChildRules(inner =>
+                    {
+                        inner
+                            .RuleFor(e => e)
+                            .Matches(RegexUtils.Hour)
+                            .WithMessage(e => MessageExtensions.IsNotAMatch("hour", e, RegexUtils.HourRule));
+                    });
             });
     }
 }
