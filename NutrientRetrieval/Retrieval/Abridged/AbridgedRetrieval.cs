113b44e956a53abc5f1e8084a497dc70494cb343
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-12-24 01:43:15 -0300

    Added insertion methods to the nutrient retrieval variants

--- NutrientRetrieval/Retrieval/Abridged/AbridgedRetrieval.cs
+++ NutrientRetrieval/Retrieval/Abridged/AbridgedRetrieval.cs
@@ -1,9 +1,14 @@
+using Domain.Models;
 using NutrientRetrieval.Food.Abridged;
 
 namespace NutrientRetrieval.Retrieval.Abridged;
 
 public static class AbridgedRetrieval
 {
-    public static async Task RetrieveFromApi() =>
+    public static async Task BatchGetNutrients() =>
         await FoodRetrieval.BatchGetNutrients<Food.Abridged.Food, FoodNutrient>();
+    
+    public static async Task BatchGetNutrients(
+        NutrifoodsDbContext context, IList<(int FdcId, Ingredient Ingredient)> tuples) =>
+        await FoodRetrieval.BatchGetNutrients<Food.Abridged.Food, FoodNutrient>(context, tuples);
 }
--- NutrientRetrieval/Retrieval/Full/FullRetrieval.cs
+++ NutrientRetrieval/Retrieval/Full/FullRetrieval.cs
@@ -1,8 +1,14 @@
+using Domain.Models;
 using NutrientRetrieval.Food.Full;
 
 namespace NutrientRetrieval.Retrieval.Full;
 
 public static class FullRetrieval
 {
-    public static async Task RetrieveFromApi() => await FoodRetrieval.BatchGetNutrients<Food.Full.Food, FoodNutrient>();
+    public static async Task BatchGetNutrients() =>
+        await FoodRetrieval.BatchGetNutrients<Food.Full.Food, FoodNutrient>(RetrievalMethod.Full);
+
+    public static async Task BatchGetNutrients(
+        NutrifoodsDbContext context, IList<(int FdcId, Ingredient Ingredient)> tuples) =>
+        await FoodRetrieval.BatchGetNutrients<Food.Full.Food, FoodNutrient>(context, tuples, RetrievalMethod.Full);
 }
