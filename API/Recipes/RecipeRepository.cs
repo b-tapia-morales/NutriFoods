867b581aecde5bca8428de5d0842fde21c90d745
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-12-24 15:16:02 -0300

    More formatting

--- API/Recipes/RecipeRepository.cs
+++ API/Recipes/RecipeRepository.cs
@@ -29,102 +29,80 @@ public class RecipeRepository : IRecipeRepository
         _context = context;
     }
 
-    public async Task<RecipeDto?> FindById(int id)
-    {
-        var recipe = await _context.Recipes.FindFirstBy(e => e.Id == id);
-        return _mapper.Map<RecipeDto>(recipe);
-    }
+    public async Task<RecipeDto?> FindById(int id) =>
+        _mapper.Map<RecipeDto>(await _context.Recipes.FindFirstBy(e => e.Id == id));
 
-    public async Task<RecipeDto?> FindByNameAndAuthor(string name, string author)
-    {
-        var recipe = await _context.Recipes.FindFirstBy(e => NormalizeStr(e.Name).Equals(NormalizeStr(name)) &&
-                                                             NormalizeStr(e.Author).Equals(NormalizeStr(author)));
-        Console.WriteLine(recipe == null);
-        return _mapper.Map<RecipeDto>(recipe);
-    }
+    public async Task<RecipeDto?> FindByNameAndAuthor(string name, string author) =>
+        _mapper.Map<RecipeDto>(await _context.Recipes
+            .FindFirstBy(e => NormalizeStr(e.Name).Equals(NormalizeStr(name)) &&
+                              NormalizeStr(e.Author).Equals(NormalizeStr(author)))
+        );
 
-    public async Task<RecipeDto?> FindByUrl(string url)
-    {
-        var recipe = await _context.Recipes.FindFirstBy(e => e.Url.ToLower().Equals(url.ToLower()));
-        return _mapper.Map<RecipeDto>(recipe);
-    }
+    public async Task<RecipeDto?> FindByUrl(string url) =>
+        _mapper.Map<RecipeDto>(await _context.Recipes.FindFirstBy(e => e.Url.ToLower().Equals(url.ToLower())));
 
-    public async Task<List<RecipeDto>> FindAll(int pageNumber, int pageSize)
-    {
-        var recipes = await _context.Recipes
+    public async Task<List<RecipeDto>> FindAll(int pageNumber, int pageSize) =>
+        _mapper.Map<List<RecipeDto>>(await _context.Recipes
             .FindAllBy(e => e.Id > ((pageNumber - 1) * pageSize) && e.Id <= (pageNumber * pageSize))
-            .ToListAsync();
-        return _mapper.Map<List<RecipeDto>>(recipes);
-    }
+            .ToListAsync()
+        );
 
-    public async Task<List<RecipeDto>> FindOrderedBy(Nutrients nutrient, int pageNumber, int pageSize, bool descending)
-    {
-        var recipes = await _context.Recipes
+    public async Task<List<RecipeDto>> FindOrderedBy(Nutrients nutrient, int pageNumber, int pageSize,
+        bool descending) =>
+        _mapper.Map<List<RecipeDto>>(await _context.Recipes
             .FindAllBy(e => e.NutritionalValues.Any(x => x.Nutrient == nutrient))
             .SortedBy(e => e.NutritionalValues.First(x => x.Nutrient == nutrient).Quantity, !descending)
             .Paginate(pageNumber, pageSize)
-            .ToListAsync();
-        return _mapper.Map<List<RecipeDto>>(recipes);
-    }
+            .ToListAsync()
+        );
 
     public async Task<List<RecipeDto>> FindByMealType(MealTypes mealType, int pageNumber, int pageSize)
     {
-        var recipes = await _context.Recipes
+        return _mapper.Map<List<RecipeDto>>(await _context.Recipes
             .FindAllBy(e => e.MealTypes.Contains(mealType))
             .Paginate(pageNumber, pageSize)
-            .ToListAsync();
-        return _mapper.Map<List<RecipeDto>>(recipes);
+            .ToListAsync()
+        );
     }
 
-    public async Task<List<RecipeDto>> FindByDishType(DishTypes dishType, int pageNumber, int pageSize)
-    {
-        var recipes = await _context.Recipes
+    public async Task<List<RecipeDto>> FindByDishType(DishTypes dishType, int pageNumber, int pageSize) =>
+        _mapper.Map<List<RecipeDto>>(await _context.Recipes
             .FindAllBy(e => e.DishTypes.Contains(dishType))
             .Paginate(pageNumber, pageSize)
-            .ToListAsync();
-        return _mapper.Map<List<RecipeDto>>(recipes);
-    }
+            .ToListAsync()
+        );
 
-    public async Task<List<RecipeDto>> GetVegetarianRecipes(Diets diet, int pageNumber, int pageSize)
-    {
-        var inconsumableGroups = diet.InconsumableGroups;
-        var recipes = await _context.Recipes
+    public async Task<List<RecipeDto>> GetVegetarianRecipes(Diets diet, int pageNumber, int pageSize) =>
+        _mapper.Map<List<RecipeDto>>(await _context.Recipes
             .FindAllBy(e => e.RecipeMeasures.Select(x => x.IngredientMeasure.Ingredient)
-                .All(x => !(diet == Vegan && x.IsAnimal) && !inconsumableGroups.Contains(x.FoodGroup)))
+                .All(x => !(diet == Vegan && x.IsAnimal) && !diet.InconsumableGroups.Contains(x.FoodGroup)))
             .FindAllBy(e => e.RecipeQuantities.Select(x => x.Ingredient)
-                .All(x => !(diet == Vegan && x.IsAnimal) && !inconsumableGroups.Contains(x.FoodGroup)))
+                .All(x => !(diet == Vegan && x.IsAnimal) && !diet.InconsumableGroups.Contains(x.FoodGroup)))
             .Paginate(pageNumber, pageSize)
-            .ToListAsync();
-        return _mapper.Map<List<RecipeDto>>(recipes);
-    }
+            .ToListAsync()
+        );
 
     public async Task<List<RecipeDto>> FindByPreparationTime(int lowerBound, int upperBound, int pageNumber,
-        int pageSize)
-    {
-        var recipes = await _context.Recipes
+        int pageSize) =>
+        _mapper.Map<List<RecipeDto>>(await _context.Recipes
             .FindAllBy(e => e.Time != null && e.Time >= lowerBound && e.Time <= upperBound)
             .Paginate(pageNumber, pageSize)
-            .ToListAsync();
-        return _mapper.Map<List<RecipeDto>>(recipes);
-    }
+            .ToListAsync()
+        );
 
-    public async Task<List<RecipeDto>> FindByPortions(int portions, int pageNumber, int pageSize)
-    {
-        var recipes = await _context.Recipes
+    public async Task<List<RecipeDto>> FindByPortions(int portions, int pageNumber, int pageSize) =>
+        _mapper.Map<List<RecipeDto>>(await _context.Recipes
             .FindAllBy(e => e.Portions != null && e.Portions == portions)
             .Paginate(pageNumber, pageSize)
-            .ToListAsync();
-        return _mapper.Map<List<RecipeDto>>(recipes);
-    }
+            .ToListAsync()
+        );
 
-    public async Task<List<RecipeDto>> FindByPortions(int lowerBound, int upperBound, int pageNumber, int pageSize)
-    {
-        var recipes = await _context.Recipes
+    public async Task<List<RecipeDto>> FindByPortions(int lowerBound, int upperBound, int pageNumber, int pageSize) =>
+        _mapper.Map<List<RecipeDto>>(await _context.Recipes
             .FindAllBy(e => e.Portions != null && e.Portions >= lowerBound && e.Portions <= upperBound)
             .Paginate(pageNumber, pageSize)
-            .ToListAsync();
-        return _mapper.Map<List<RecipeDto>>(recipes);
-    }
+            .ToListAsync()
+        );
 
     public Task<List<RecipeDto>> FindByEnergy(int lowerBound, int upperBound) =>
         FilterByNutrientQuantity(Energy, lowerBound, upperBound);
@@ -137,18 +115,6 @@ public class RecipeRepository : IRecipeRepository
 
     public Task<List<RecipeDto>> FindByProteins(int lowerBound, int upperBound) =>
         FilterByNutrientQuantity(Proteins, lowerBound, upperBound);
-
-    public async Task<List<RecipeDto>> FilterByMacronutrientDistribution(double energy, double carbohydrates,
-        double fattyAcids, double proteins)
-    {
-        var recipes = await _context.Recipes
-            .FindAllBy(e => e.NutritionalValues.Any(x => x.Nutrient == Energy && x.Quantity <= energy))
-            .FindAllBy(e => e.NutritionalValues.Any(x => x.Nutrient == Carbohydrates && x.Quantity <= carbohydrates))
-            .FindAllBy(e => e.NutritionalValues.Any(x => x.Nutrient == FattyAcids && x.Quantity <= fattyAcids))
-            .FindAllBy(e => e.NutritionalValues.Any(x => x.Nutrient == Proteins && x.Quantity <= proteins))
-            .ToListAsync();
-        return _mapper.Map<List<RecipeDto>>(recipes);
-    }
 
     public async Task<RecipeLogging> InsertRecipe(MinimalRecipe recipe)
     {
