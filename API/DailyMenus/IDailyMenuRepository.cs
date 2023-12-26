003b13731ebc9b7a362309019882edee1c36ec5c
b-tapia-morales <b-tapia-morales@users.noreply.github.com>
2023-12-26 00:36:10 -0300

    Fixed bug: now plan generation is fully parallelizable

--- API/DailyMenus/IDailyMenuRepository.cs
+++ API/DailyMenus/IDailyMenuRepository.cs
@@ -13,17 +13,21 @@ public interface IDailyMenuRepository
     Task<DailyMenuDto> GenerateMenu(DailyMenuDto dailyMenu) =>
         GenerateMenu(dailyMenu, ToValue(dailyMenu.MealType));
 
-    async IAsyncEnumerable<DailyMenuDto> ToTasks(List<DailyMenuDto> menus, bool useFilter = true)
+    async IAsyncEnumerable<Task<DailyMenuDto>> ToTasks(List<DailyMenuDto> menus, bool useFilter = true)
     {
         foreach (var menu in menus)
-            yield return await GenerateMenu(menu, useFilter ? ToValue(menu.MealType) : MealTypes.None);
+            yield return GenerateMenu(menu, useFilter ? ToValue(menu.MealType) : MealTypes.None);
     }
 
     async Task<IEnumerable<DailyMenuDto>> Parallelize(List<DailyMenuDto> menus, bool useFilter = true)
     {
         var queue = new ConcurrentQueue<DailyMenuDto>();
         var tasks = ToTasks(menus, useFilter);
-        await tasks.AsyncParallelForEach(e => Task.Run(() => { queue.Enqueue(e); }));
+        await tasks.AsyncParallelForEach(async e =>
+        {
+            var menu = await e;
+            queue.Enqueue(menu);
+        });
         return queue;
     }
 }
